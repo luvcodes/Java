8de1f8ef0d51ec99b4d5b5f89c1d99f580744cee
ryan0028 <ryan0028@student.monash.edu>
2022-11-20 17:16:39 +1100

    Packages usage and modifiers

---
+++ src/Basics/level3/modifier/Test.java
@@ -0,0 +1,10 @@
+package Basics.level3.modifier;
+
+public class Test {
+    public static void main(String[] args) {
+        A a = new A();
+        a.m1();
+        B b = new B();
+        b.say();
+    }
+}
