d29bea1688e04105bdb89cf720bfddd2b75be6d7
Runze Yang <ryan0028@student.monash.edu>
2022-12-13 09:18:45 +1100

    零钱通OOP实现方式

---
+++ src/Basics/smallChanges/SmallChangeSysApp.java
@@ -0,0 +1,13 @@
+package Basics.smallChanges;
+
+import Basics.smallChanges.oop.SmallChangeSysOOP;
+
+/**
+ * 调用相关方法，完成功能
+ * */
+public class SmallChangeSysApp {
+    public static void main(String[] args) {
+        System.out.println("======Hello Company======");
+        new SmallChangeSysOOP().mainMenu();
+    }
+}
