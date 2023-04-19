8919022857c62208dc2d5693afb93804f24c6f45
ryan0028 <ryan0028@student.monash.edu>
2023-04-19 10:59:17 +1000

    Main method functionalities

---
+++ src/Advance/MainMethod/Main02.java
@@ -0,0 +1,9 @@
+package Advance.MainMethod;
+
+public class Main02 {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("args[" + i + "] = " + args[i]);
+        }
+    }
+}
