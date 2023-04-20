20823d7c65bb872cd9c21cc8f71aa5c9deb90619
Runze Yang <ryan0028@student.monash.edu>
2023-04-20 12:34:55 +1000

    Final keyword detail

--- src/Advance/final_/FinalDetail01.java
+++ src/Advance/final_/FinalDetail01.java
@@ -2,7 +2,8 @@ package Advance.final_;
 
 public class FinalDetail01 {
     public static void main(String[] args) {
-
+        CC cc = new CC();
+        new EE().cal();
     }
 }
 
@@ -23,3 +24,29 @@ class AA {
         TAX_RATE3 = 8.8;
     }
 }
+
+class BB {
+    /**
+     * 如果final修饰的属性是静态的，则初始化的位置只能是
+     * 1. 定义时
+     * 2. 在静态代码块 不能在构造器中赋值
+     */
+
+    public static final double TAX_RATE = 99.9;
+    public static final double TAX_RATE2;
+
+    static {
+        TAX_RATE2 = 3.3;
+    }
+}
+
+// final类不能继承，但是可以实例化对象
+final class CC {}
+// 如果类不是final类，但是含有final方法，则该方法虽然不能重写，但是可以被继承
+class DD {
+    public final void cal() {
+        System.out.println("cal() method");
+    }
+}
+
+class EE extends DD{}
