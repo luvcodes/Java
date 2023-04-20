563c4612e9a1d69735e38368e28ce1cb6a74042e
Runze Yang <ryan0028@student.monash.edu>
2023-04-21 00:30:12 +1000

    Template design pattern update

--- src/Advance/DesignPattern/TemplatePattern/BB.java
+++ src/Advance/DesignPattern/TemplatePattern/BB.java
@@ -1,18 +1,11 @@
 package Advance.DesignPattern.TemplatePattern;
 
-public class BB {
-    public void calculateTime() {
-        // 得到开始的时间
-        long start = System.currentTimeMillis();
-        job();
-        // 得到结束的时间
-        long end = System.currentTimeMillis();
-        System.out.println("AA 执行时间: " + (end - start));
-    }
+public class BB extends Template{
 
-    public void job() {
+    @Override
+    public void job() { // 这里也去充血了Template的job方法
         long num = 0;
-        for (int i = 0; i <= 80000; i++) {
+        for (int i = 0; i <= 100000; i++) {
             num *= i;
         }
     }
--- src/Advance/DesignPattern/TemplatePattern/Template.java
+++ src/Advance/DesignPattern/TemplatePattern/Template.java
@@ -3,13 +3,13 @@ package Advance.DesignPattern.TemplatePattern;
 abstract public class Template {
     public abstract void job(); // 抽象方法
 
-    public void calculateTime() {
+    public void calculateTime() { // 实现方法，调用job()方法
         // 得到开始的时间
         long start = System.currentTimeMillis();
-        job();
+        job(); // 动态绑定机制
         // 得到结束的时间
         long end = System.currentTimeMillis();
-        System.out.println("AA 执行时间: " + (end - start));
+        System.out.println("任务执行时间: " + (end - start));
 
     }
 }
