a26cc3a9ba933f1cc30492f62bb09a057c59120a
ryan0028 <ryan0028@student.monash.edu>
2023-04-24 11:23:50 +1000

    Enumeration in JAVA

---
+++ src/PhaseOne/enum_/Enumeration02.java
@@ -0,0 +1,47 @@
+package PhaseOne.enum_;
+
+public class Enumeration02 {
+    public static void main(String[] args) {
+        System.out.println(Season.SPRING);
+        System.out.println(Season.SUMMER);
+        System.out.println(Season.AUTUMN);
+        System.out.println(Season.WINTER);
+    }
+}
+
+
+class Season {
+    private String name;
+    private String description;
+
+    // 定义了四个对象
+    public final static Season SPRING = new Season("Spring", "Warm");
+    public final static Season SUMMER = new Season("Summer", "Hot");
+    public final static Season AUTUMN = new Season("Autumn", "Cool");
+    public final static Season WINTER = new Season("Winter", "Cold");
+
+    // 1. 将构造器私有化，目的防止 直接 new
+    // 2. 去掉setXxx方法，防止属性被修改
+    // 3. 在Season内部，直接创建固定的对象
+    // 4. 优化，可以加入 final 修饰符
+    private Season(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return "Season{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
---
+++ src/PhaseOne/enum_/Enumeration03.java
@@ -0,0 +1,43 @@
+package PhaseOne.enum_;
+
+public class Enumeration03 {
+    public static void main(String[] args) {
+        System.out.println(Season2.SPRING);
+        System.out.println(Season2.SUMMER);
+        System.out.println(Season2.AUTUMN);
+        System.out.println(Season2.WINTER);
+    }
+}
+
+enum Season2 {
+    // 如果使用enum 来实现枚举类
+    // 1. 使用关键字enum替代class
+    // 2. public final static Season SPRING = new Season2("Spring", "Warm");
+    // SPRING("Spring", "Warm"); 常量名(实参列表)
+    // 3. 如果有多个常量(对象), 使用逗号间隔
+    // 4. 如果使用enum来实现枚举，要求将定义敞亮对象，写在最前面
+    SPRING("Spring", "Warm"), WINTER("Winter", "Cold"), AUTUMN("Autumn", "Cool"), SUMMER("Summer", "Hot");
+    private String name;
+    private String description;
+
+    private Season2(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return "Season{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
