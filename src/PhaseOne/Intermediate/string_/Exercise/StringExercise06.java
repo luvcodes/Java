53fd7d28c816eaf4455ff865f8088314d945af52
ryan0028 <ryan0028@student.monash.edu>
2023-04-26 20:51:03 +1000

    String type exercises

---
+++ src/PhaseOne/Intermediate/string_/Exercise/StringExercise06.java
@@ -0,0 +1,15 @@
+package PhaseOne.Intermediate.string_.Exercise;
+
+public class StringExercise06 {
+    public static void main(String[] args) {
+        // 以下语句创建了几个对象？
+        String s1 = "hello";
+        s1 = "haha";
+        /**
+         * 创建了两个对象。
+         * s1在字符串常量池中查找hello，没有就创建
+         * 然后s1在字符串常量池中查找haha，没有就创建
+         */
+    }
+}
+
---
+++ src/PhaseOne/Intermediate/string_/Exercise/StringExercise08.java
@@ -0,0 +1,21 @@
+package PhaseOne.Intermediate.string_.Exercise;
+
+public class StringExercise08 {
+    public static void main(String[] args) {
+        String a = "hello";
+        String b = "abc";
+        /**
+         * 1. 先创建要给StringBuilder sb = StringBUilder();
+         * 2. 执行 sb.append("hello");
+         * 3. 执行 sb.append("abc");
+         * 4. 调用 String s = sb.toString();
+         * 最后其实是 c 指向堆中的对象(String) value[] -> 池中 "helloabc"
+         * 一共有3个对象
+         */
+        String c = a + b;
+        String d = "helloabc";
+        // System.out.println(c == d); // false
+        String e = "hello" + "abc"; // e指向常量池
+        System.out.println(d == e); // true
+    }
+}
