3b8ab8fb0c9724c5874541d8492815cbc0c9ccf4
Ryan Yang <ryan0028@student.monash.edu>
2023-07-11 20:14:26 +1000

    update folder structure

--- src/PhaseOne/Intermediate/Exception_/CommonExceptions_/NullPointerException_.java
+++
@@ -1,8 +0,0 @@
-package PhaseOne.Intermediate.Exception_.CommonExceptions_;
-
-public class NullPointerException_ {
-    public static void main(String[] args) {
-        String name = null;
-        System.out.println(name.length());
-    }
-}
