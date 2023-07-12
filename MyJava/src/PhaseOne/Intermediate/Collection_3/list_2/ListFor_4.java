7ecba5642fc28988d0fc7904a7bfb64bee4c0f85
Ryan Yang <ryan0028@student.monash.edu>
2023-07-12 18:57:48 +1000

    List的三种遍历方式

---
+++ MyJava/src/PhaseOne/Intermediate/Collection_3/list_2/ListFor_4.java
@@ -0,0 +1,44 @@
+package PhaseOne.Intermediate.Collection_3.list_2;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ListFor_4 {
+    @SuppressWarnings({"all"})
+    public static void main(String[] args) {
+
+        //List 接口的实现子类 Vector LinkedList
+        //List list = new ArrayList();
+        //List list = new Vector();
+        List list = new LinkedList();
+
+        list.add("jack");
+        list.add("tom");
+        list.add("鱼香肉丝");
+        list.add("北京烤鸭子");
+
+        //遍历
+        //1. 迭代器
+        Iterator iterator = list.iterator();
+        while (iterator.hasNext()) {
+            Object obj =  iterator.next();
+            System.out.println(obj);
+
+        }
+
+        System.out.println("=====增强for=====");
+        //2. 增强for
+        for (Object o : list) {
+            System.out.println("o=" + o);
+        }
+
+        System.out.println("=====普通for====");
+        //3. 使用普通for
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println("对象=" + list.get(i));
+        }
+
+
+    }
+}
