7b2c5064aab3099ae6085f55c5f96de5103dea30
Runze Yang <ryan0028@student.monash.edu>
2023-07-22 16:29:19 +1000

    Generics

---
+++ MyJava/src/PhaseOne/Intermediate/Generic_/Generic03.java
@@ -0,0 +1,34 @@
+package PhaseOne.Intermediate.Generic_;
+
+public class Generic03 {
+    public static void main(String[] args) {
+        /**
+         * <p>
+         *     1. 其实这样相当于就是吧下面所有的E的地方，都改成了String类型
+         *     2. 修改成Integer，那么就是所有的E的地方的都改成了Integer类型
+         *     3. 注意: E具体的数据类型在定义Person对象的时候制定，即在编译期间，就确定E的类型
+         * </p>
+         * */
+        Person<String> stringPerson = new Person<String>("hsp education");
+        stringPerson.show();
+
+        Person<Integer> stringPerson2 = new Person<Integer>(100);
+        stringPerson2.show();
+    }
+}
+
+class Person<E> {
+    E s; // E表示 s的数据类型，该数据类型是在定义Person对象的时候指定的，即在编译期间，就确定E是什么类型
+
+    // E也可以是参数类型
+    public Person(E s) {
+        this.s = s;
+    }
+    // 返回类型使用E
+    public E f() {
+        return s;
+    }
+    public void show() {
+        System.out.println(s.getClass());
+    }
+}
---
+++ MyJava/src/PhaseOne/Intermediate/Generic_/GenericExercise.java
@@ -0,0 +1,54 @@
+package PhaseOne.Intermediate.Generic_;
+
+import java.util.*;
+
+@SuppressWarnings({"all"})
+public class GenericExercise {
+    public static void main(String[] args) {
+        /**
+         * 使用泛型方式给HashSet 放入3个学生对象
+         * */
+        Set<Student> students = new HashSet<Student>();
+        students.add(new Student("ryan", 22));
+        students.add(new Student("mark", 21));
+        students.add(new Student("bill", 25));
+        // iterate
+        for (Student student : students) {
+            System.out.println(student);
+        }
+        System.out.println();
+
+        /**
+         * 使用泛型方式给HashMap 放入三个学生对象
+         * */
+        HashMap studentHashMap = new HashMap<String, Student>();
+        studentHashMap.put("ryan", new Student("ryan", 22));
+        studentHashMap.put("mark", new Student("mark", 21));
+        studentHashMap.put("bill", new Student("bill", 25));
+        // iterate
+        Set<Map.Entry<String, Student>> entries = studentHashMap.entrySet();
+        Iterator<Map.Entry<String, Student>> iterator = entries.iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, Student> next =  iterator.next();
+            System.out.println(next.getKey() + " - " + next.getValue());
+        }
+    }
+}
+
+class Student {
+    public String name;
+    public int age;
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
