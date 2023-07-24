ce85023c35635fda038ca9e3468a81cec90d6f90
Runze Yang <ryan0028@student.monash.edu>
2023-07-24 17:15:51 +1000

    update on folder structure

--- MyJava/src/PhaseOne/Intermediate/List_4/List_1.java
+++ MyJava/src/PhaseOne/Intermediate/List_4/List_1.java
@@ -6,12 +6,13 @@ import java.util.List;
 @SuppressWarnings({ "all" })
 public class List_1 {
     public static void main(String[] args) {
-        // 1. List集合类中元素有序(即添加顺序和取出顺序一致)、且可重复 [案例]
+        // 1. List集合类中元素有序(即添加顺序和取出顺序一致)、且可重复
         List list = new ArrayList();
         list.add("jack");
         list.add("tom");
         list.add("mary");
         list.add("hsp");
+        // 再add一个tom也没问题
         list.add("tom");
         System.out.println("list=" + list);
         for (Object i : list) {
@@ -20,6 +21,6 @@ public class List_1 {
         System.out.println();
         // 2. List集合中的每个元素都有其对应的顺序索引，即支持索引
         // 索引是从0开始的
-        System.out.println(list.get(3));// hsp
+        System.out.println(list.get(3));
     }
 }
