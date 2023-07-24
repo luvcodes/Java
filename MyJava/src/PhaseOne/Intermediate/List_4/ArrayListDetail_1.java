8e5c947e5fd725638931f5a7918aa8dffff35da0
luvcodes <88066423+luvcodes@users.noreply.github.com>
2023-07-24 22:29:40 +1000

    update folder structure

--- MyJava/src/PhaseOne/Intermediate/List_4/ArrayListDetail_1.java
+++ MyJava/src/PhaseOne/Intermediate/List_4/ArrayListDetail_1.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 
 public class ArrayListDetail_1 {
     public static void main(String[] args) {
+        /**
+         * 1. ArrayList可以加入null，并且多个
+         * 2. ArrayList是由数组来实现数据存储的
+         * 3. ArrayList基本等于Vector，没有synchronized修饰，线程不安全
+         */
         ArrayList arrayList = new ArrayList();
         arrayList.add(null);
         arrayList.add("jack");
