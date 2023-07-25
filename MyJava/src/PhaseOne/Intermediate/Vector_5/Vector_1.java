b12877aeefa72d1e5be82405986d34e1d5a8f9c3
Runze Yang <ryan0028@student.monash.edu>
2023-07-26 09:53:20 +1000

    README update

--- MyJava/src/PhaseOne/Intermediate/Vector_5/Vector_1.java
+++ MyJava/src/PhaseOne/Intermediate/Vector_5/Vector_1.java
@@ -1,14 +1,14 @@
-package PhaseOne.Intermediate.Collection_3;
+package PhaseOne.Intermediate.Vector_5;
 
-import java.util.Vector;
+import java.util.*;
 
 public class Vector_1 {
     public static void main(String[] args) {
         // 无参构造器
-        Vector vector = new Vector();
-        for (int i = 0; i < 10; i++) {
-            vector.add(i);
-        }
+//        Vector vector = new Vector();
+//        for (int i = 0; i < 10; i++) {
+//            vector.add(i);
+//        }
         // 有参数的构造
         Vector vector = new Vector(8);
         for (int i = 0; i < 10; i++) {
