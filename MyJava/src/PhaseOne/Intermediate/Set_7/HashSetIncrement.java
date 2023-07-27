4d45c2265f2d69eb5c48eebb7de86a20579cfba7
Runze Yang <ryan0028@student.monash.edu>
2023-07-27 10:17:05 +1000

    HashSetIncrement update

--- MyJava/src/PhaseOne/Intermediate/Set_7/HashSetIncrement.java
+++ MyJava/src/PhaseOne/Intermediate/Set_7/HashSetIncrement.java
@@ -8,6 +8,7 @@ public class HashSetIncrement {
         for (int i = 1; i <= 12; i++) {
             hashSet.add(new A(i));
         }
+        System.out.println("hashSet = " + hashSet);
     }
 }
 
@@ -15,5 +16,14 @@ class A {
     private int n;
     public A(int n) {
         this.n = n;
+    }
+    /**
+     * 这样重写hashCode方法是因为，在新的数据想要加入到hashSet(HashMap)中的时候，源码中经历了一步转变为hashCode的方法来确定input数据目标
+     * 加入的位置，那通过重写HashCode方法，就可以指定让新的数据都定义为hashCode值是100，也就可以将所有的新数据都加在同一条链表上了。讲这里的时候
+     * 是为了体现红黑树的生成，也就是为了满足同一条链表上有八个节点的情况。
+     * */
+    @Override
+    public int hashCode() {
+        return 100;
     }
 }
