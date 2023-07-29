c59089d0abfcd4ff594277b7b5563aeaf02c0c52
Runze Yang <ryan0028@student.monash.edu>
2023-07-29 17:04:09 +1000

    Thread in Java

--- MyJava/src/PhaseOne/Amatuer/ThreadUse/Thread01.java
+++ MyJava/src/PhaseOne/Amatuer/ThreadUse/Thread01.java
@@ -5,9 +5,9 @@ public class Thread01 {
 
         //创建Cat对象，可以当做线程使用
         Cat cat = new Cat();
-        //老韩读源码
+
         /*
-            (1)
+            (1) 追源码 start方法
             public synchronized void start() {
                 start0();
             }
@@ -15,20 +15,23 @@ public class Thread01 {
             //start0() 是本地方法，是JVM调用, 底层是c/c++实现
             //真正实现多线程的效果， 是start0(), 而不是 run
             private native void start0();
-
          */
 
-        cat.start();//启动线程-> 最终会执行cat的run方法
+        // 启动线程 -> 最终会执行cat的run方法
+        cat.start();
 
-//        //cat.run();//run方法就是一个普通的方法, 没有真正的启动一个线程，就会把run方法执行完毕，才向下执行
-//        //说明: 当main线程启动一个子线程 Thread-0, 主线程不会阻塞, 会继续执行
-//        //这时 主线程和子线程是交替执行..
-//        System.out.println("主线程继续执行" + Thread.currentThread().getName());//名字main
-//        for(int i = 0; i < 60; i++) {
-//            System.out.println("主线程 i=" + i);
-//            //让主线程休眠
-//            Thread.sleep(1000);
-//        }
+        //run方法就是一个普通的方法, 如果不先执行start方法而直接执行run方法，就相当于没有真正的启动一个线程，就会把run方法执行完毕，才向下执行
+        //cat.run();
+        /**
+         *  说明: 当main线程启动一个子线程 Thread-0, 主线程不会阻塞, 会继续执行
+         *  这时 主线程和子线程是交替执行..
+         */
+        System.out.println("主线程继续执行" + Thread.currentThread().getName());//名字main
+        for(int i = 0; i < 60; i++) {
+            System.out.println("主线程 i=" + i);
+            //让主线程休眠
+            Thread.sleep(1000);
+        }
     }
 }
 
@@ -44,14 +47,10 @@ public class Thread01 {
         }
     }
  */
-
-
 class Cat extends Thread {
-
     int times = 0;
     @Override
     public void run() {//重写run方法，写上自己的业务逻辑
-
         while (true) {
             //该线程每隔1秒。在控制台输出 “喵喵, 我是小猫咪”
             System.out.println("喵喵, 我是小猫咪" + (++times) + " 线程名=" + Thread.currentThread().getName());
