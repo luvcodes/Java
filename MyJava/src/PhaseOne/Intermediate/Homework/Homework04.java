a18b242a34f360b721acc38c2e4b4bb1f43e43a0
Runze Yang <ryan0028@student.monash.edu>
2023-07-30 22:37:02 +1000

    Homework for Collection

---
+++ MyJava/src/PhaseOne/Intermediate/Homework/Homework04.java
@@ -0,0 +1,15 @@
+package PhaseOne.Intermediate.Homework;
+
+import java.util.TreeSet;
+
+public class Homework04 {
+    public static void main(String[] args) {
+        TreeSet treeSet = new TreeSet();
+        treeSet.add("hsp");
+        treeSet.add("tom");
+        treeSet.add("king");
+        treeSet.add("hsp");//加入不了
+        System.out.println(treeSet);
+
+    }
+}
