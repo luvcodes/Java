a0ab8516b47af18b1dfa9298af987889689cd6fd
Runze Yang <ryan0028@student.monash.edu>
2023-08-04 00:34:56 +1000

    FileWriter, README update

--- MyJava/src/PhaseOne/Amatuer/writer_/FileWriter_.java
+++ MyJava/src/PhaseOne/Amatuer/writer_/FileWriter_.java
@@ -9,7 +9,6 @@ import java.io.IOException;
  */
 public class FileWriter_ {
     public static void main(String[] args) {
-
         String filePath = "e:\\note.txt";
         //创建FileWriter对象
         FileWriter fileWriter = null;
@@ -28,16 +27,11 @@ public class FileWriter_ {
 //            7) write(string,off,len):写入字符串的指定部分
             fileWriter.write("上海天津", 0, 2);
             //在数据量大的情况下，可以使用循环操作.
-
-
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-
             //对应FileWriter , 一定要关闭流，或者flush才能真正的把数据写入到文件
-            //老韩看源码就知道原因.
             /*
-                看看代码
                 private void writeBytes() throws IOException {
         this.bb.flip();
         int var1 = this.bb.limit();
