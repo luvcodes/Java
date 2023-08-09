08048f90ff20f404ccbc73fab164fc7f97f7b9aa
Runze Yang <ryan0028@student.monash.edu>
2023-08-09 10:02:08 +1000

    Equals method and object parameter pass method

---
+++ MyJava/src/PhaseOne/Basics/level3/objectEquals/Finalize.java
@@ -0,0 +1,29 @@
+package PhaseOne.Basics.level3.objectEquals;
+
+public class Finalize {
+    public static void main(String[] args) {
+        Car bmw = new Car("BMW");
+        // 这时car对象就是一个垃圾对象，垃圾回收器就会回(销毁)对象，在销毁对象前，会调用该对象的finalize()方法
+        // 程序员就可以在finalize中，写自己的业务逻辑代码(比如释放资源，数据库的连接，或者打开文件...)
+        // 如果程序员不重写 finalize方法，那么就会调用Object类的finalize，即默认处理
+        // 如果程序员重写了 finalize，就可以实现自己的逻辑
+        bmw = null;
+        System.gc(); // 主动调用垃圾回收器
+        System.out.println("程序退出了...");
+    }
+}
+
+class Car {
+    private String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    // 重写finalize
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("我们销毁 汽车" + name);
+        System.out.println("释放了某些资源...");
+    }
+}
---
+++ MyJava/src/PhaseOne/Basics/level3/objectEquals/HashCode.java
@@ -0,0 +1,24 @@
+package PhaseOne.Basics.level3.objectEquals;
+
+/**
+ * 提高具有哈希结构的容器的效率
+ * 两个引用，如果指向的是同一个对象，则哈希值一定是一样的
+ * 两个引用，如果指向的是不同对象，则哈希值是不一样的
+ * 哈希值主要根据地址号来的，不能完全将哈希值等价于地址
+ * 见以下的案例演示
+ * 后面在集合中hashCode如果需要的话，也会重写
+ * */
+public class HashCode {
+    public static void main(String[] args) {
+        AA aa1 = new AA();
+        AA aa2 = new AA();
+        AA aa3 = aa1;
+        System.out.println("aa.hashCode() = " + aa1.hashCode());
+        System.out.println("aa.hashCode() = " + aa2.hashCode());
+        System.out.println("aa.hashCode() = " + aa3.hashCode());
+    }
+}
+
+class AA {
+
+}
