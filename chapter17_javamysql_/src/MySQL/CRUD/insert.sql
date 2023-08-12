76d53022e833a6ea93d223f27e6ab16ad13b67c1
Ryan Yang <ryan0028@student.monash.edu>
2023-08-12 12:46:25 +1000

    insert statement and update statement

---
+++ chapter17_javamysql_/src/MySQL/CRUD/insert.sql
@@ -0,0 +1,14 @@
+-- 练习insert 语句
+CREATE TABLE `goods` (
+    id INT,
+    goods_name VARCHAR(10),
+    price DOUBLE
+);
+-- 添加数据
+INSERT INTO `goods` (id, goods_name, price)
+VALUES (10, '华为手机', 2000);
+INSERT INTO `goods` (id, goods_name, price)
+VALUES (20, '苹果手机', 3000);
+DESC goods;
+SELECT *
+FROM goods;
