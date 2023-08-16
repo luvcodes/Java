e4f2db7a3980410f50af65fa9c485f025924c89d
Ryan Yang <ryan0028@student.monash.edu>
2023-08-16 15:32:12 +1000

    operators in select statement

--- chapter17_javamysql_/src/MySQL/BasicKeyword/IsNullKeyword.sql
+++
@@ -1,7 +0,0 @@
--- USE sql_store;
--- 
--- SELECT * FROM customers WHERE phone is null;
--- -- SELECT * FROM customers WHERE phone IS NOT NULL;
-
-USE sql_store;
-SELECT * FROM orders WHERE shipper_id is NULL;
