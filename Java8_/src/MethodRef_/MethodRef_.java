46b3ca44252c186ce11c902765e5225397380f96
Runze Yang <ryan0028@student.monash.edu>
2023-08-30 12:49:09 +1000

    Java 8新特性之方法引用

--- Java8_/src/MethodRef_/MethodRef_.java
+++ Java8_/src/MethodRef_/MethodRef_.java
@@ -3,7 +3,10 @@ package MethodRef_;
 import org.junit.jupiter.api.Test;
 
 import java.io.PrintStream;
+import java.util.Comparator;
+import java.util.function.BiPredicate;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 /**
@@ -46,6 +49,79 @@ public class MethodRef_ {
         System.out.println("*************");
         Supplier<String> sup2 = emp::getName;
         System.out.println(sup2);
+    }
+
+
+    /**
+     * 情况2: 类 :: 静态方法
+     * */
+
+    @Test
+    public void test3() {
+        Comparator<Integer> comparator = (t1, t2) -> Integer.compare(t1, t2);
+        System.out.println(comparator.compare(12, 21));
+
+        System.out.println("*******************");
+        Comparator<Integer> comparator1 = Integer::compare;
+        System.out.println(comparator1.compare(12, 3));
+    }
+
+
+    // Function中的R apply(T t)
+    // Math中的Long round(Double d)
+    @Test
+    public void test4() {
+        Function<Double, Long> func1 = new Function<Double, Long>() {
+            @Override
+            public Long apply(Double aDouble) {
+                return Math.round(aDouble);
+            }
+        };
+        System.out.println("********************");
+        Function<Double, Long> func2 = d -> Math.round(d);
+        System.out.println(func2.apply(12.3));
+        System.out.println("********************");
+        Function<Double, Long> func3 = Math::round;
+        System.out.println(func3.apply(12.6));
+    }
+
+     /**
+      * 情况三: 类 :: 实例方法
+      * Comparator中的int compare(T t1, T t2)
+      * String中的int t1.compareTo(t2)
+      */
+    @Test
+    public void test5() {
+        Comparator<String> comparator = (s1, s2) -> s1.compareTo(s2);
+        System.out.println(comparator.compare("abc", "abd"));
+        System.out.println("****************");
+        Comparator<String> comparator1 = String :: compareTo;
+        // -9 c减m的ascii码得到的
+        System.out.println(comparator1.compare("abc", "abm"));
+    }
+
+    // BiPredicate中的boolean test(T t1, T t2);
+    // String中的boolean t1.equals(t2)
+    @Test
+    public void test6() {
+        BiPredicate<String, String> predicate = (s1, s2) -> s1.equals(s2);
+        System.out.println(predicate.test("abc", "abc"));
+
+        System.out.println("****************");
+        BiPredicate<String, String> predicate1 = String :: equals;
+        System.out.println(predicate1.test("abc", "abd"));
+    }
+
+    // Function中的R apply(T t)
+    // Employee中的String getName();
+    @Test
+    public void test7() {
+        Employee mark = new Employee(10, "mark", 23, 6000);
+        Function<Employee, String> function = emp -> emp.getName();
+        System.out.println(function.apply(mark));
+        System.out.println("*****************");
+        Function<Employee, String> function1 = Employee::getName;
+        System.out.println(function1.apply(mark));
     }
 }
 
