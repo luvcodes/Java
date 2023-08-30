e5d1c90715843167663a8393f6fb20bcea1d287c
Ryan Yang <ryan0028@student.monash.edu>
2023-08-30 11:41:44 +1000

    函数式接口

--- Java8_/src/Lambda_/funcInterface_/FunctionalInterface_.java
+++ Java8_/src/Lambda_/funcInterface_/FunctionalInterface_.java
@@ -2,7 +2,11 @@ package Lambda_.funcInterface_;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 public class FunctionalInterface_ {
     /**
@@ -15,12 +19,12 @@ public class FunctionalInterface_ {
 
     @Test
     public void test1() {
-//        happyTime(500, new Consumer<Double>() {
-//            @Override
-//            public void accept(Double aDouble) {
-//                System.out.println("矿泉水价格为: " + aDouble);
-//            }
-//        });
+        happyTime(500, new Consumer<Double>() {
+            @Override
+            public void accept(Double aDouble) {
+                System.out.println("矿泉水价格为: " + aDouble);
+            }
+        });
 
         System.out.println("**********************");
         happyTime(400, money -> System.out.println("矿泉水价格为: " + money));
@@ -28,5 +32,41 @@ public class FunctionalInterface_ {
 
     public void happyTime(double money, Consumer<Double> consumer) {
         consumer.accept(money);
+    }
+
+    @Test
+    public void test2() {
+        List<String> list = new ArrayList<>();
+        list.add("beijing");
+        list.add("najing");
+        list.add("tianjin");
+        List<String> filterStr = filterString(list, new Predicate<String>() {
+            @Override
+            public boolean test(String s) {
+                return s.contains("jing");
+            }
+        });
+
+        System.out.println(filterStr);
+
+
+        System.out.println("******************************");
+        // Lambda表达式
+        List<String> filterStrs1 = filterString(list, s -> s.contains("jing"));
+        System.out.println(filterStrs1);
+    }
+
+    /**
+     * 根据给定的规则，过滤集合中的字符创。此规则由Predicate的方法决定
+     * */
+    public List<String> filterString(List<String> list, Predicate<String> predicate) {
+        ArrayList<String> filterList = new ArrayList<>();
+        for (String s : list) {
+            if (predicate.test(s)) {
+                filterList.add(s);
+            }
+        }
+
+        return filterList;
     }
 }
