b607eef785229294d819e5269e1db60f0294bea3
Ryan Yang <ryan0028@student.monash.edu>
2023-09-07 14:51:58 +1000

    update on the mysql content

--- Java入门/chapter17_javamysql_/src/MySQL/17.29 表查询-加强/LIMIT.sql
+++ Java入门/chapter17_javamysql_/src/MySQL/17.29 表查询-加强/LIMIT.sql
@@ -1,16 +1,19 @@
 USE sql_store;
 
 -- 6, 3 表示跳过前6个，取第7~9个，6是偏移量
--- SELECT * FROM customers
--- LIMIT 6,3;
+SELECT * FROM emp
+LIMIT 6,3;
 
--- SELECT * FROM customers
--- LIMIT 3;
--- 
--- SELECT * FROM customers
--- LIMIT 300;
+SELECT * FROM emp
+LIMIT 3;
 
-SELECT * FROM customers
+SELECT * FROM emp
+LIMIT 300;
+
+SELECT * FROM emp
 (where)
-order by points desc
+order by empno desc
 limit 3;
+
+
+select * from emp;
