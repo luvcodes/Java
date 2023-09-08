ba09c34191ae6c215cf454b6ce9ef4802d5a07ae
Ryan Yang <ryan0028@student.monash.edu>
2023-09-08 14:30:35 +1000

    update on the trasaction and view

--- Java入门/chapter17_javamysql_/src/MySQL/17.42 mysql视图/view.sql
+++ Java入门/chapter17_javamysql_/src/MySQL/17.42 mysql视图/view.sql
@@ -7,13 +7,13 @@ CREATE VIEW emp_view01
 	SELECT empno, ename, job, deptno FROM emp; 
 
 -- 查看视图
-DESC emp_view01
+DESC emp_view01;
 
 SELECT * FROM emp_view01;
 SELECT empno, job  FROM emp_view01;
 
 -- 查看创建视图的指令
-SHOW CREATE VIEW emp_view01
+SHOW CREATE VIEW emp_view01;
 -- 删除视图
 DROP VIEW emp_view01;
 
@@ -27,26 +27,24 @@ DROP VIEW emp_view01;
 
 UPDATE emp_view01 
 	SET job = 'MANAGER' 
-	WHERE empno = 7369
+	WHERE empno = 7369;
 	
 SELECT * FROM emp; -- 查询基表
 
 
-SELECT * FROM emp_view01
+SELECT * FROM emp_view01;
 
 -- 修改基本表， 会影响到视图
 
 UPDATE emp 
 	SET job = 'SALESMAN' 
-	WHERE empno = 7369
+	WHERE empno = 7369;
 
 -- 3. 视图中可以再使用视图 , 比如从emp_view01 视图中，选出empno,和ename做出新视图
-DESC emp_view01
+DESC emp_view01;
 
 CREATE VIEW emp_view02
 	AS
-	SELECT empno, ename FROM emp_view01
+	SELECT empno, ename FROM emp_view01;
 	
-SELECT * FROM emp_view02
-
-
+SELECT * FROM emp_view02;
