29c281288401ec53d6be2da59f349929ee1f029d
Ryan Yang <ryan0028@student.monash.edu>
2023-09-09 13:39:29 +1000

    maven project update

---
+++ JavaWeb02/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package com.ryanMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
