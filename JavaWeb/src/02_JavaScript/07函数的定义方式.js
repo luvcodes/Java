90ebd7f58c2adf61aa4519e5e29194b155f55627
Runze Yang <ryan0028@student.monash.edu>
2023-09-16 17:22:29 +1000

    函数的定义方式

---
+++ JavaWeb/src/02_JavaScript/07函数的定义方式.js
@@ -0,0 +1,15 @@
+// 定义一个无参函数
+function func() {
+    let a = 0;
+    if (a) {
+        console.log("true");
+    } else {
+        console.log("false");
+    }
+}
+
+// 定义带有返回值的函数
+function sum(num1, nums2) {
+    let result = num1 + nums2;
+    return result;
+}
