95031ead9a3cd5b441ff2b7cd19768d0da7de784
Ryan Yang <ryan0028@student.monash.edu>
2023-09-17 13:34:13 +1000

    table组件

--- JavaWeb/src/Vue_/vue-project/src/App.vue
+++ JavaWeb/src/Vue_/vue-project/src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
-    <h1>{{ message }}</h1>
+    <!-- <h1>{{ message }}</h1> -->
+    <element-view></element-view>
   </div>
 </template>
 
 <script>
+import ElementView from '../src/views/element/ElementView.vue';
   export default {
+    components: {ElementView},
     data() {
       return {
         message: "Hello Vue",
---
+++ JavaWeb/src/Vue_/vue-project/src/views/element/ElementView.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
+    <br /><br />
+
+    <el-table
+        :data="tableData"
+        border
+        style="width: 100%">
+      <el-table-column
+          prop="date"
+          label="日期"
+          width="180">
+      </el-table-column>
+      <el-table-column
+          prop="name"
+          label="姓名"
+          width="180">
+      </el-table-column>
+      <el-table-column
+          prop="address"
+          label="地址">
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
+    }
+  }
+};
+</script>
+
+<style></style>
