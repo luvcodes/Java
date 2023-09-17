353f2eb691df76a41fb1a15dffa24a63a42eb45c
Ryan Yang <ryan0028@student.monash.edu>
2023-09-17 14:22:13 +1000

    ElementView

--- JavaWeb/src/Vue_/vue-project/src/views/element/ElementView.vue
+++ JavaWeb/src/Vue_/vue-project/src/views/element/ElementView.vue
@@ -10,25 +10,21 @@
     </el-row>
     <br /><br />
 
-    <el-table
-        :data="tableData"
-        border
-        style="width: 100%">
-      <el-table-column
-          prop="date"
-          label="日期"
-          width="180">
-      </el-table-column>
-      <el-table-column
-          prop="name"
-          label="姓名"
-          width="180">
-      </el-table-column>
-      <el-table-column
-          prop="address"
-          label="地址">
-      </el-table-column>
+    <!-- Table表格 -->
+    <el-table :data="tableData" border style="width: 100%">
+      <el-table-column prop="date" label="日期" width="180"> </el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"> </el-table-column>
+      <el-table-column prop="address" label="地址"> </el-table-column>
     </el-table>
+
+    <!-- Pagination分页 -->
+    <el-pagination
+      background
+      layout="total, sizes, prev, pager, next, jumper"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :total="1000"
+    ></el-pagination>
   </div>
 </template>
 
@@ -36,24 +32,37 @@
 export default {
   data() {
     return {
-      tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄'
-      }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
-      }]
-    }
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄",
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄",
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄",
+        },
+      ],
+    };
+  },
+  methods: {
+    handleSizeChange: function(val) {
+        alert("每页记录数变化" + val);
+    },
+    handleCurrentChange: function(val) {
+        alert("页码发生变化" + val);
+    },
   }
 };
 </script>
