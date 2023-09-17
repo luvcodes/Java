b432bd681a492cc5d3dbae1ee3172c607a997de0
Ryan Yang <ryan0028@student.monash.edu>
2023-09-17 14:40:18 +1000

    ElementUI常见组件

--- JavaWeb/src/Vue_/vue-project/src/views/element/ElementView.vue
+++ JavaWeb/src/Vue_/vue-project/src/views/element/ElementView.vue
@@ -25,6 +25,68 @@
       @current-change="handleCurrentChange"
       :total="1000"
     ></el-pagination>
+
+    <br /><br />
+
+    <!-- Dialog对话框 -->
+    <!-- Table -->
+    <el-button type="text" @click="dialogTableVisible = true"
+      >打开嵌套表格的 Dialog</el-button
+    >
+    <el-dialog title="收货地址" :visible.sync="dialogTableVisible">
+      <el-table :data="gridData">
+        <el-table-column
+          property="date"
+          label="日期"
+          width="150"
+        ></el-table-column>
+        <el-table-column
+          property="name"
+          label="姓名"
+          width="200"
+        ></el-table-column>
+        <el-table-column property="address" label="地址"></el-table-column>
+      </el-table>
+    </el-dialog>
+
+    <el-button type="text" @click="dialogFormVisible = true"
+      >打开嵌套form的 Dialog</el-button
+    >
+    <el-dialog title="收货地址" :visible.sync="dialogFormVisible">
+      <el-form ref="form" :model="form" label-width="80px">
+        <el-form-item label="活动名称">
+          <el-input v-model="form.name"></el-input>
+        </el-form-item>
+        <el-form-item label="活动区域">
+          <el-select v-model="form.region" placeholder="请选择活动区域">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="活动时间">
+          <el-col :span="11">
+            <el-date-picker
+              type="date"
+              placeholder="选择日期"
+              v-model="form.date1"
+              style="width: 100%"
+            ></el-date-picker>
+          </el-col>
+          <el-col class="line" :span="2">-</el-col>
+          <el-col :span="11">
+            <el-time-picker
+              placeholder="选择时间"
+              v-model="form.date2"
+              style="width: 100%"
+            ></el-time-picker>
+          </el-col>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">立即创建</el-button>
+          <el-button>取消</el-button>
+        </el-form-item>
+      </el-form>
+    </el-dialog>
   </div>
 </template>
 
@@ -32,6 +94,36 @@
 export default {
   data() {
     return {
+      form: {
+        name: "",
+        region: "",
+        date1: "",
+        date2: "",
+      },
+      gridData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+      ],
+      dialogTableVisible: false,
+      dialogFormVisible: false,
       tableData: [
         {
           date: "2016-05-02",
@@ -57,13 +149,17 @@ export default {
     };
   },
   methods: {
-    handleSizeChange: function(val) {
-        alert("每页记录数变化" + val);
+    handleSizeChange: function (val) {
+      alert("每页记录数变化" + val);
     },
-    handleCurrentChange: function(val) {
-        alert("页码发生变化" + val);
+    handleCurrentChange: function (val) {
+      alert("页码发生变化" + val);
     },
-  }
+    onSubmit() {
+        // 如果不用JSON.stringify方法，那么输出出来的其实就是Object object
+      alert(JSON.stringify(this.form))
+    },
+  },
 };
 </script>
 
