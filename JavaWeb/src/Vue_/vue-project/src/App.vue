7d84806dce4d36166856945f99a32ae284c2fe5e
Ryan Yang <ryan0028@student.monash.edu>
2023-09-17 13:09:50 +1000

    前端工程化

--- JavaWeb/src/Vue_/vue-project/src/App.vue
+++ JavaWeb/src/Vue_/vue-project/src/App.vue
@@ -1,32 +1,19 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+  <div>
+    <h1>{{ message }}</h1>
   </div>
 </template>
 
+<script>
+  export default {
+    data() {
+      return {
+        message: "Hello Vue",
+      }
+    },
+  }
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
