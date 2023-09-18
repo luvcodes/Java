d54e2397b03a6d85f9600b079b72ebc26f01d88e
Ryan Yang <ryan0028@student.monash.edu>
2023-09-18 10:15:05 +1000

    Update on the Vue project router

--- JavaWeb/src/Vue_/vue-project/src/App.vue
+++ JavaWeb/src/Vue_/vue-project/src/App.vue
@@ -1,14 +1,25 @@
 <template>
   <div>
     <!-- <h1>{{ message }}</h1> -->
-    <element-view></element-view>
+    <!-- <element-view></element-view> -->
+
+    <!-- 员工管理 -->
+    <emp-view></emp-view>
   </div>
 </template>
 
 <script>
-import ElementView from '../src/views/element/ElementView.vue';
+// import ElementView from '../src/views/element/ElementView.vue';
+import EmpView from '../src/views/tlias/EmpView.vue';
   export default {
-    components: {ElementView},
+    components: {
+      EmpView
+      
+      // ElementView
+    
+    
+    
+    },
     data() {
       return {
         message: "Hello Vue",
