8b539627d66ff1468e9a8a83e2204b31c39974dc
Ryan Yang <ryan0028@student.monash.edu>
2023-09-23 16:54:41 +1000

    MyBatis使用resultMap

--- mybatis-demo04/src/test/java/com/itheima/test/MyBatisTest.java
+++ mybatis-demo04/src/test/java/com/itheima/test/MyBatisTest.java
@@ -1,6 +1,7 @@
 package com.itheima.test;
 
 import com.itheima.mapper.BrandMapper;
+import com.itheima.pojo.Brand;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -9,6 +10,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 public class MyBatisTest {
     @Test
@@ -22,7 +24,10 @@ public class MyBatisTest {
         SqlSession sqlSession = sqlSessionFactory.openSession();
 
         // 3. 获取Mapper接口的代理对象
-        BrandMapper brands = sqlSession.getMapper(BrandMapper.class);
+        BrandMapper brandMapper = sqlSession.getMapper(BrandMapper.class);
+
+        // 4. 执行方法
+        List<Brand> brands = brandMapper.selectAll();
         System.out.println(brands);
 
         // 4. 释放资源
