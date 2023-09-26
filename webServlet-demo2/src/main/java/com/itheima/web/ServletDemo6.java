8e2d747645e8edfca36c224b7ef1e8bcce53ca95
Ryan Yang <ryan0028@student.monash.edu>
2023-09-26 23:52:51 +1000

    Servlet demo for post and get method

---
+++ webServlet-demo2/src/main/java/com/itheima/web/ServletDemo6.java
@@ -0,0 +1,43 @@
+package com.itheima.web;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class ServletDemo6 implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        // 根据请求方式的不同，进行分别的处理
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+
+
+        // 1. 获取请求方式
+        String method = request.getMethod();
+        // 2. 判断
+        if ("GET".equals(method)) {
+            // GET方式的处理逻辑
+        } else if ("POST".equals(method)) {
+            // POST方式的处理逻辑
+        }
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
