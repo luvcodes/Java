6cdcf0895548348077353699d86051102b5daa3b
Ryan Yang <ryan0028@student.monash.edu>
2023-10-02 17:02:30 +1100

    Dependency injection初次体验

--- spring_01_quickstart/src/main/java/com/itheima/service/impl/BookServiceImpl.java
+++ spring_01_quickstart/src/main/java/com/itheima/service/impl/BookServiceImpl.java
@@ -4,9 +4,18 @@ import com.itheima.dao.impl.BookDao;
 import com.itheima.dao.impl.BookDaoImpl;
 
 public class BookServiceImpl implements BookService {
-    private BookDao bookDao = new BookDaoImpl();
+    // 5. 删除业务层中使用new的方式创建的DAO对象
+//    private BookDao bookDao = new BookDaoImpl();
+    private BookDao bookDao;
     public void save() {
         System.out.println("book service save...");
         bookDao.save();
+    }
+
+    // 6. 提供setter方法
+    // 这个setter方法是容器在执行
+    public void setBookDao(BookDao bookDao) {
+        this.bookDao = bookDao;
+        System.out.println("calling the set method");
     }
 }
