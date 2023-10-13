42a6519b706f7d5bd135f2dfc2c17db128c103b6
Ryan Yang <ryan0028@student.monash.edu>
2023-10-13 13:26:58 +1100

    SpringMVC JSON format and Date format as parameters

--- springmvc/springmvc_04_request_param/src/main/java/com/itheima/config/SpringMVCConfig.java
+++ springmvc/springmvc_04_request_param/src/main/java/com/itheima/config/SpringMVCConfig.java
@@ -2,8 +2,10 @@ package com.itheima.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan("com.itheima.controller")
+@EnableWebMvc // 开启由JSON数据转换成对象的功能
 public class SpringMVCConfig {
 }
--- springmvc/springmvc_04_request_param/src/main/java/com/itheima/controller/UserController.java
+++ springmvc/springmvc_04_request_param/src/main/java/com/itheima/controller/UserController.java
@@ -1,8 +1,17 @@
 package com.itheima.controller;
 
+import com.itheima.domain.User;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -12,5 +21,75 @@ public class UserController {
         System.out.println("普通参数传递 name ==> " + name);
         System.out.println("普通参数传递 age ==> " + age);
         return "{'module':'commonParam'}";
+    }
+
+    @RequestMapping("/commonParamDifferentName")
+    @ResponseBody
+    public String commonParamDifferentName(@RequestParam("name") String userName, int age) {
+        System.out.println("普通参数传递 name ==> " + userName);
+        System.out.println("普通参数传递 age ==> " + age);
+        return "{'module':'common param different name'}";
+    }
+
+    @RequestMapping("/pojoParam")
+    @ResponseBody
+    public String pojoParam(User user) {
+        System.out.println("pojo参数传递 user ==> " + user);
+        return "{'module':'pojo param'}";
+    }
+
+    // 这个是为了处理User里面如果带有引用属性，就是有Address类的实例对象，怎么处理
+    @RequestMapping("/pojoContainPojoParam")
+    @ResponseBody
+    public String pojoContainPojoParam(User user) {
+        System.out.println("pojo嵌套pojo参数传递 user ==> " + user);
+        return "{'module':'pojoContainPojo param'}";
+    }
+
+    @RequestMapping("/arrayParam")
+    @ResponseBody
+    public String arrayParam(String[] likes) {
+        System.out.println("数组参数传递 ==> " + Arrays.toString(likes));
+        return "{'module':'array param'}";
+    }
+
+    @RequestMapping("/listParam")
+    @ResponseBody
+    public String listParam(@RequestParam List<String> likes) {
+        System.out.println("集合参数传递 ==> " + likes);
+        return "{'module':'list param'}";
+    }
+
+    // 集合参数 JSON格式
+    @RequestMapping("/listParamForJson")
+    @ResponseBody
+    public String listParamForJson(@RequestBody List<String> likes) {
+        System.out.println("list common(json)参数传递 list ==> " + likes);
+        return "{'module':'list common for json param'}";
+    }
+
+    @RequestMapping("/pojoParamForJson")
+    @ResponseBody
+    public String PojoParamForJson(@RequestBody User user) {
+        System.out.println("pojo(json)参数传递 user ==> " + user);
+        return "{'module':'user pojo for json param'}";
+    }
+
+    @RequestMapping("/listPojoParamForJson")
+    @ResponseBody
+    public String listPojoParamForJson(@RequestBody List<User> list) {
+        System.out.println("list pojo(json)参数传递 user ==> " + list);
+        return "{'module':'list user pojo for json param'}";
+    }
+
+    @RequestMapping("/dataParam")
+    @ResponseBody
+    public String dataParam(Date date,
+                            @DateTimeFormat(pattern = "yyyy-MM-dd") Date date1,
+                            @DateTimeFormat(pattern = "yyyy/MM/dd HH:mm:ss") Date date2) {
+        System.out.println("date参数传递 date ==> " + date);
+        System.out.println("date1参数传递 date ==> " + date1);
+        System.out.println("date2参数传递 date ==> " + date2);
+        return "{'module':'date param'}";
     }
 }
