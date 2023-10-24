dea5f87b8be394643632f75a73477beea8ca309c
Ryan Yang <ryan0028@student.monash.edu>
2023-10-24 23:15:17 +1100

    SpringMVC interceptor setup

---
+++ springmvc/springmvc_12_interceptor/src/main/java/com/itheima/config/SpringMvcConfig.java
@@ -0,0 +1,13 @@
+package com.itheima.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@ComponentScan({"com.itheima.controller", "com.itheima.config"})
+@EnableWebMvc
+public class SpringMvcConfig {
+
+}
