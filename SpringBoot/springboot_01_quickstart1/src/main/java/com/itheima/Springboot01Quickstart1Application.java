ee513482ae173c99118c457e5180bb60e34f2b37
Ryan Yang <ryan0028@student.monash.edu>
2023-10-28 08:53:20 +1100

    springboot initialization

---
+++ SpringBoot/springboot_01_quickstart1/src/main/java/com/itheima/Springboot01Quickstart1Application.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot01Quickstart1Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot01Quickstart1Application.class, args);
+    }
+}
