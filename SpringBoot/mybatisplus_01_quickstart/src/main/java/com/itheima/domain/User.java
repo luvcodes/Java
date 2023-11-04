7d7d58f66d93f0f97510b93c1b46dd96c2ce365e
Ryan Yang <ryan0028@student.monash.edu>
2023-11-04 15:42:46 +1100

    MyBatis Plus update for lombok jar package

--- SpringBoot/mybatisplus_01_quickstart/src/main/java/com/itheima/domain/User.java
+++ SpringBoot/mybatisplus_01_quickstart/src/main/java/com/itheima/domain/User.java
@@ -1,5 +1,8 @@
 package com.itheima.domain;
 
+import lombok.*;
+
+@Data
 public class User {
     private Long id;
     private String name;
@@ -7,54 +10,4 @@ public class User {
     private Integer age;
     private String tel;
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", password='" + password + '\'' +
-                ", age=" + age +
-                ", tel='" + tel + '\'' +
-                '}';
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getTel() {
-        return tel;
-    }
-
-    public void setTel(String tel) {
-        this.tel = tel;
-    }
 }
--- SpringBoot/mybatisplus_01_quickstart/src/test/java/com/itheima/Mybatisplus01QuickstartApplicationTests.java
+++ SpringBoot/mybatisplus_01_quickstart/src/test/java/com/itheima/Mybatisplus01QuickstartApplicationTests.java
@@ -13,6 +13,35 @@ class Mybatisplus01QuickstartApplicationTests {
     @Autowired
     private UserDao userDao;
 
+    @Test
+    void testSave() {
+        User user = new User();
+        user.setName("后端程序员");
+        user.setPassword("123456");
+        user.setAge(12);
+        user.setTel("1006184000");
+        userDao.insert(user);
+    }
+
+    @Test
+    void testDelete() {
+        userDao.deleteById(1720659325802409985L);
+    }
+
+    @Test
+    void testUpdate() {
+        User user = new User();
+        user.setId(1L);
+        user.setName("update");
+
+        userDao.updateById(user);
+    }
+
+    @Test
+    void testGetById() {
+        User user = userDao.selectById(2L);
+        System.out.println(user);
+    }
 
     @Test
     void testGetAll() {
