1f3c4a81b888d351443429cfcd09f78702c7fbc5
Ryan Yang <ryan0028@student.monash.edu>
2023-12-01 12:22:09 +1100

    setup ssmp project for springboot

---
+++ SpringBoot/springboot_09_ssmp/src/main/java/com/itheima/SSMPApplication.java
@@ -0,0 +1,12 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot09SsmpApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot09SsmpApplication.class, args);
+    }
+
+}
