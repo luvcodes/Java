7a1dc5b930a0a5a8d1c2ff3fb4bdaf589d069c87
Ryan Yang <ryan0028@student.monash.edu>
2023-12-02 00:04:45 +1100

    create the datasource exercise project for spring

---
+++ Spring/spring_09_datasource/src/main/java/com/itheima/App.java
@@ -0,0 +1,19 @@
+package com.itheima;
+
+import com.itheima.dao.BookDao;
+import javax.sql.DataSource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class App {
+    public static void main(String[] args) {
+//        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+//        DataSource dataSource = (DataSource) ctx.getBean("dataSource");
+//        System.out.println(dataSource);
+
+         ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+         BookDao bookDao = (BookDao) ctx.getBean("bookDao");
+         bookDao.save();
+    }
+}
