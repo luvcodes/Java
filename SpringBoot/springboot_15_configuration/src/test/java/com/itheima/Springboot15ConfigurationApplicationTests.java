bd5f46abcff02cd143bf6a2d70697ce005d928a1
Ryan Yang <ryan0028@student.monash.edu>
2023-12-07 12:40:05 +1100

    SpringBoot configuration of 宽松绑定内容更新

--- SpringBoot/springboot_15_configuration/src/test/java/com/itheima/Springboot15ConfigurationApplicationTests.java
+++ SpringBoot/springboot_15_configuration/src/test/java/com/itheima/Springboot15ConfigurationApplicationTests.java
@@ -1,13 +1,16 @@
 package com.itheima;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Springboot15ConfigurationApplicationTests {
+    @Value("${servers.ip-address}")
+    private String message;
 
     @Test
     void contextLoads() {
+        System.out.println(message);
     }
-
 }
