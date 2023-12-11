98445175935bd844bfc1fdc4f366c4075179efcf
Ryan Yang <ryan0028@student.monash.edu>
2023-12-11 23:11:38 +1100

    SpringBoot MyImportSelector

--- SpringBoot/springboot_29_bean_init/src/main/java/com/itheima/bean/MyImportSelector.java
+++ SpringBoot/springboot_29_bean_init/src/main/java/com/itheima/bean/MyImportSelector.java
@@ -3,9 +3,25 @@ package com.itheima.bean;
 import org.springframework.context.annotation.ImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.Map;
+
 public class MyImportSelector implements ImportSelector {
     @Override
-    public String[] selectImports(AnnotationMetadata annotationMetadata) {
-        return new String[]{"com.itheima.bean.Dog", "com.itheima.bean.Cat"};
+    public String[] selectImports(AnnotationMetadata metadata) {
+//        System.out.println("================================================");
+//        System.out.println("提示: " + metadata.getClassName()); // SpringConfig6
+//        System.out.println(metadata.hasAnnotation("org.springframework.context.annotation.Configuration"));
+//        System.out.println("================================================");
+//        Map<String, Object> attributes = metadata.getAnnotationAttributes("org.springframework.context.annotation.ComponentScan");
+//        System.out.println(attributes);
+//        System.out.println("================================================");
+
+
+        // 各种条件的判定，判定完毕后，决定是否装在指定的bean
+        boolean hasAnnotation = metadata.hasAnnotation("org.springframework.context.annotation.Configuration");
+        if (hasAnnotation) {
+            return new String[]{"com.itheima.bean.Dog"};
+        }
+        return new String[]{"com.itheima.bean.Cat"};
     }
 }
--- SpringBoot/springboot_29_bean_init/src/main/java/com/itheima/config/SpringConfig6.java
+++ SpringBoot/springboot_29_bean_init/src/main/java/com/itheima/config/SpringConfig6.java
@@ -1,9 +1,12 @@
 package com.itheima.config;
 
-import com.itheima.bean.Dog;
 import com.itheima.bean.MyImportSelector;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+@Configuration
+//@ComponentScan(basePackages = "com.itheima")
 @Import(MyImportSelector.class)
 public class SpringConfig6 {
 }
