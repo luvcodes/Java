e95d9fd100bc51e55206e2a99fa23bb93fe995c2
Ryan Yang <ryan0028@student.monash.edu>
2024-01-04 17:05:04 +1100

    Redis serializer

---
+++ PhaseFour/Redis/redis-demo1/src/main/java/org/ryanyang/redis/pojo/User.java
@@ -0,0 +1,13 @@
+package org.ryanyang.redis.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String name;
+    private Integer age;
+}
--- PhaseFour/Redis/redis-demo1/src/test/java/org/ryanyang/RedisDemo1ApplicationTests.java
+++ PhaseFour/Redis/redis-demo1/src/test/java/org/ryanyang/RedisDemo1ApplicationTests.java
@@ -1,6 +1,7 @@
 package org.ryanyang;
 
 import org.junit.jupiter.api.Test;
+import org.ryanyang.redis.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -8,12 +9,22 @@ import org.springframework.data.redis.core.RedisTemplate;
 @SpringBootTest
 class RedisDemo1ApplicationTests {
     @Autowired
-    private RedisTemplate redisTemplate;
+    private RedisTemplate<String, Object> redisTemplate;
+
     @Test
-    void contextLoads() {
+    void testString() {
         redisTemplate.opsForValue().set("name", "mark");
 
         Object name = redisTemplate.opsForValue().get("name");
-        System.out.println("name" + name);
+        System.out.println("name = " + name);
+    }
+
+    @Test
+    void testSaveUser() {
+        redisTemplate.opsForValue().set("user:100", new User("ryan", 21));
+
+        // 获取数据
+        User o = (User) redisTemplate.opsForValue().get("user:100");
+        System.out.println("o = " + o);
     }
 }
