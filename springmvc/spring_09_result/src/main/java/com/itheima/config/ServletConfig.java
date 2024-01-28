86742f17aa4e97be5e80f73f9747dcaeea567bbb
Ryan Yang <ryan0028@student.monash.edu>
2024-01-28 23:09:24 +1100

    Collections content update

--- springmvc/spring_09_result/src/main/java/com/itheima/config/ServletConfig.java
+++ springmvc/spring_09_result/src/main/java/com/itheima/config/ServletConfig.java
@@ -3,14 +3,17 @@ package com.itheima.config;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class ServletConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
     protected Class<?>[] getRootConfigClasses() {
         return new Class[]{SpringConfig.class};
     }
 
+    @Override
     protected Class<?>[] getServletConfigClasses() {
         return new Class[]{SpringMvcConfig.class};
     }
 
+    @Override
     protected String[] getServletMappings() {
         return new String[]{"/"};
     }
--- springmvc/springmvc_08_ssm/src/main/java/com/itheima/service/BookServiceImpl.java
+++ springmvc/springmvc_08_ssm/src/main/java/com/itheima/service/BookServiceImpl.java
@@ -4,7 +4,6 @@ import com.itheima.dao.BookDao;
 import com.itheima.domain.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 
