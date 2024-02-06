f287aa116226cfe019447af355ce15392376fc78
Ryan Yang <ryan0028@student.monash.edu>
2024-02-06 14:52:39 +1100

    Reflection demo 1 file update

--- JavaSE/chapter16_Reflection_/src/myreflect/src/com/itheima/myreflect1/MyReflectDemo1.java
+++ JavaSE/chapter16_Reflection_/src/myreflect/src/com/itheima/myreflect1/MyReflectDemo1.java
@@ -5,15 +5,6 @@ package com.itheima.myreflect1;
  */
 public class MyReflectDemo1 {
     public static void main(String[] args) throws ClassNotFoundException {
-
-        /*
-        * 获取class对象的三种方式：
-        *   1. Class.forName("全类名");
-        *   2. 类名.class
-        *   3. 对象.getClass();
-        * */
-
-
         //1. 第一种方式
         //全类名 ： 包名 + 类名
         //最为常用的
