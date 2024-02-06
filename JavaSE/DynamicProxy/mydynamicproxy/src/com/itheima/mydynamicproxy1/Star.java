cbf024101c57c473a3378dc3c13b01c2e1bc8539
Ryan Yang <ryan0028@student.monash.edu>
2024-02-07 00:10:20 +1100

    Dynamic proxy for JavaSE

---
+++ JavaSE/DynamicProxy/mydynamicproxy/src/com/itheima/mydynamicproxy1/Star.java
@@ -0,0 +1,14 @@
+package com.itheima.mydynamicproxy1;
+
+public interface Star {
+
+    //我们可以把所有想要被代理的方法定义在接口当中
+
+    //唱歌
+    public abstract String sing(String name);
+
+    //跳舞
+    public abstract void dance();
+
+
+}
