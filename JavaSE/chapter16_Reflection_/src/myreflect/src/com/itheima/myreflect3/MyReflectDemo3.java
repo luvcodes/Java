2d58baba07772882b12ddc4ed1d40cd71b90b173
Ryan Yang <ryan0028@student.monash.edu>
2024-02-07 09:03:58 +1100

    Another update on the reflection chapter contents

--- JavaSE/chapter16_Reflection_/src/myreflect/src/com/itheima/myreflect3/MyReflectDemo3.java
+++ JavaSE/chapter16_Reflection_/src/myreflect/src/com/itheima/myreflect3/MyReflectDemo3.java
@@ -8,12 +8,6 @@ import java.lang.reflect.Field;
 public class MyReflectDemo3 {
     public static void main(String[] args) throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException {
     /*
-       Class类中用于获取成员变量的方法
-            Field[] getFields()：                返回所有公共成员变量对象的数组
-            Field[] getDeclaredFields()：        返回所有成员变量对象的数组
-            Field getField(String name)：        返回单个公共成员变量对象
-            Field getDeclaredField(String name)：返回单个成员变量对象
-
        Field类中用于创建对象的方法
             void set(Object obj, Object value)：赋值
             Object get(Object obj)              获取值
