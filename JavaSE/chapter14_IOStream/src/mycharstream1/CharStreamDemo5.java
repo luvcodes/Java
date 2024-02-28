fae4d9e6f2e3654c146042c82e5f15aea0b3458e
Runze Yang <ryanyyyyyyy@163.com>
2024-02-29 10:15:23 +1100

    Char stream details

---
+++ JavaSE/chapter14_IOStream/src/mycharstream1/CharStreamDemo5.java
@@ -0,0 +1,25 @@
+package mycharstream1;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * @author ryanw
+ */
+public class CharStreamDemo5 {
+    public static void main(String[] args) throws IOException {
+        FileWriter fw = new FileWriter("C:\\Users\\ryanw\\IdeaProjects\\Java\\JavaSE\\chapter14_IOStream\\src\\mycharstream1\\a.txt");
+
+        fw.write("我的同学各个都很厉害");
+        fw.write("说话声音很好听");
+
+        fw.flush();
+
+        fw.write("都是人才");
+        fw.write("超爱这里哟");
+
+        fw.close();
+
+        fw.write("B站");
+    }
+}
