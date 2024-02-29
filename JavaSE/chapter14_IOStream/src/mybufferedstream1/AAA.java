1f218ac09f965c6ef1fa82f9543b8d9d7fab5a07
Runze Yang <ryanyyyyyyy@163.com>
2024-02-29 16:57:26 +1100

    Buffer stream

---
+++ JavaSE/chapter14_IOStream/src/mybufferedstream1/AAA.java
@@ -0,0 +1,9 @@
+package mybufferedstream1;
+
+import java.io.File;
+
+public class AAA {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ JavaSE/chapter14_IOStream/src/mybufferedstream1/BufferedStreamDemo4.java
@@ -0,0 +1,28 @@
+package mybufferedstream1;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class BufferedStreamDemo4 {
+    public static void main(String[] args) throws IOException {
+        /*
+         * 字符缓冲输出流
+         * 构造方法：
+         * public BufferedWriter(Writer r)
+         * 特有方法：
+         * public void newLine() 跨平台的换行
+         */
+        // 1. 创建字符缓冲输出流的对象
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("src/mybufferedstream1/b.txt", true));
+        // 2. 写出数据
+        bufferedWriter.newLine();
+        bufferedWriter.write("hello");
+        bufferedWriter.newLine();
+        bufferedWriter.write("world");
+        bufferedWriter.newLine();
+
+        // 3. 释放资源
+        bufferedWriter.close();
+    }
+}
