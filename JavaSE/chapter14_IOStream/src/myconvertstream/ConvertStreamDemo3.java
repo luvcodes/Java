fededa38711f81387836cbeea0371efec9dcad32
Runze Yang <ryanyyyyyyy@163.com>
2024-03-02 16:28:11 +1100

    Update on the convert stream demo 3 practice question.

--- JavaSE/chapter14_IOStream/src/myconvertstream/ConvertStreamDemo3.java
+++ JavaSE/chapter14_IOStream/src/myconvertstream/ConvertStreamDemo3.java
@@ -19,27 +19,27 @@ public class ConvertStreamDemo3 {
          */
 
          // 1. JDK 11以前的方案
-        // InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream("src/myconvertstream/b.txt"), "GBK");
-        // OutputStreamWriter outputStreamWriter = new OutputStreamWriter(new FileOutputStream("src/myconvertstream/d.txt"), "UTF-8");
+         InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream("src/myconvertstream/b.txt"), "GBK");
+         OutputStreamWriter outputStreamWriter = new OutputStreamWriter(new FileOutputStream("src/myconvertstream/d.txt"), "UTF-8");
         
-        // int b;
-        // while ((b = inputStreamReader.read()) != -1) {
-        //     outputStreamWriter.write(b);
-        // }
+         int b;
+         while ((b = inputStreamReader.read()) != -1) {
+             outputStreamWriter.write(b);
+         }
 
-        // outputStreamWriter.close();
-        // inputStreamReader.close();
+         outputStreamWriter.close();
+         inputStreamReader.close();
 
         // 2. JDK 11以后的方案
-        FileReader fileReader = new FileReader("src/myconvertstream/b.txt", Charset.forName("GBK"));
-        FileWriter fileWriter = new FileWriter("src/myconvertstream/e.txt", Charset.forName("UTF-8"));
-        
-        int b;
-        while ((b = fileReader.read()) != -1) {
-            fileWriter.write(b);
-        }
-
-        fileWriter.close();
-        fileReader.close();
+//        FileReader fileReader = new FileReader("src/myconvertstream/b.txt", Charset.forName("GBK"));
+//        FileWriter fileWriter = new FileWriter("src/myconvertstream/e.txt", Charset.forName("UTF-8"));
+//
+//        int b;
+//        while ((b = fileReader.read()) != -1) {
+//            fileWriter.write(b);
+//        }
+//
+//        fileWriter.close();
+//        fileReader.close();
     }
 }
