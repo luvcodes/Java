0fc8c098efac999efd59b6ce5c3552d2eea52b03
Runze Yang <ryanyyyyyyy@163.com>
2024-03-03 18:05:01 +1100

    Commons-io jar package and hutools jar package for IO operations

---
+++ JavaSE/chapter14_IOStream/src/myhutool11/Test1.java
@@ -0,0 +1,49 @@
+package myhutool11;
+
+import java.util.List;
+
+public class Test1 {
+    public static void main(String[] args) {
+    /*
+        FileUtil类:
+                file：根据参数创建一个file对象
+                touch：根据参数创建文件
+
+                writeLines：把集合中的数据写出到文件中，覆盖模式。
+                appendLines：把集合中的数据写出到文件中，续写模式。
+                readLines：指定字符编码，把文件中的数据，读到集合中。
+                readUtf8Lines：按照UTF-8的形式，把文件中的数据，读到集合中
+
+                copy：拷贝文件或者文件夹
+    */
+
+
+       /* File file1 = FileUtil.file("D:\\", "aaa", "bbb", "a.txt");
+        System.out.println(file1);//D:\aaa\bbb\a.txt
+
+        File touch = FileUtil.touch(file1);
+        System.out.println(touch);
+
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("aaa");
+        list.add("aaa");
+        list.add("aaa");
+
+        File file2 = FileUtil.writeLines(list, "D:\\a.txt", "UTF-8");
+        System.out.println(file2);*/
+
+      /*  ArrayList<String> list = new ArrayList<>();
+        list.add("aaa");
+        list.add("aaa");
+        list.add("aaa");
+        File file3 = FileUtil.appendLines(list, "D:\\a.txt", "UTF-8");
+        System.out.println(file3);*/
+
+
+        List<String> list = FileUtil.readLines("D:\\a.txt", "UTF-8");
+        System.out.println(list);
+
+
+    }
+}
