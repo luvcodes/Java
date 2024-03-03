6aa97f0bf2f165e8460ab1aee3d038e6f1387c2e
Runze Yang <ryanyyyyyyy@163.com>
2024-03-04 09:53:02 +1100

    Thread synchronized keyword update contents

--- JavaSE/chapter13_Thread_/src/Thread/mythread/a09threadsafe1/MyThread.java
+++ JavaSE/chapter13_Thread_/src/Thread/mythread/a09threadsafe1/MyThread.java
@@ -4,16 +4,16 @@ package Thread.mythread.a09threadsafe1;
  * @author ryanw
  */
 public class MyThread extends Thread {
-
-    // 表示这个类所有的对象，都共享ticket数据
-    // 0 ~ 99
+    // 表示这个类所有的对象，都共享ticket数据 0 ~ 99
     static int ticket = 0;
 
     @Override
     public void run() {
-            while (true) {
-                // 同步代码块
-                synchronized (MyThread.class) {
+        while (true) {
+            // 同步代码块
+            // 小括号中写锁对象，一定要是唯一的
+            // 一般可以使用当前类的字节码文件，因为在同一个文件夹里只会有一个字节码文件，所以一定是唯一的
+            synchronized (MyThread.class) {
                 if (ticket < 100) {
                     try {
                         Thread.sleep(10);
--- JavaSE/chapter13_Thread_/src/Thread/mythread/a09threadsafe1/ThreadDemo.java
+++ JavaSE/chapter13_Thread_/src/Thread/mythread/a09threadsafe1/ThreadDemo.java
@@ -6,7 +6,6 @@ package Thread.mythread.a09threadsafe1;
 public class ThreadDemo {
     public static void main(String[] args) {
        // 需求：某电影院目前正在上映国产大片，共有100张票，而它有3个窗口卖票，请设计一个程序模拟该电影院卖票
-
         MyThread t1 = new MyThread();
         MyThread t2 = new MyThread();
         MyThread t3 = new MyThread();
