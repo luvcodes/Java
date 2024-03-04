2d4b3e64f22e19bd64f6fd70790c521b84149e44
Runze Yang <ryanyyyyyyy@163.com>
2024-03-04 12:28:55 +1100

    Thread pool contents update

--- JavaSE/chapter13_Thread_/src/Thread/mythread/a15threadpool/MyThreadPoolDemo.java
+++ JavaSE/chapter13_Thread_/src/Thread/mythread/a15threadpool/MyThreadPoolDemo.java
@@ -2,13 +2,14 @@ package Thread.mythread.a15threadpool;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 public class MyThreadPoolDemo {
     public static void main(String[] args) throws InterruptedException {
-    /*
-        public static ExecutorService newCachedThreadPool()             创建一个没有上限的线程池
-        public static ExecutorService newFixedThreadPool (int nThreads) 创建有上限的线程池
-    */
+        /*
+         * public static ExecutorService newCachedThreadPool() 创建一个没有上限的线程池
+         * public static ExecutorService newFixedThreadPool (int nThreads) 创建有上限的线程池
+         */
 
         // 1.获取线程池对象
         ExecutorService pool1 = Executors.newFixedThreadPool(3);
@@ -22,6 +23,44 @@ public class MyThreadPoolDemo {
         pool1.submit(new MyRunnable());
 
         // 3.销毁线程池
-        //pool1.shutdown();
+        // pool1.shutdown();
+
+
+
+        // // 1,创建一个默认的线程池对象.池子中默认是空的.默认最多可以容纳int类型的最大值.
+        // ExecutorService executorService = Executors.newCachedThreadPool();
+        // // Executors --- 可以帮助我们创建线程池对象
+        // // ExecutorService --- 可以帮助我们控制线程池
+
+        // executorService.submit(() -> {
+        // System.out.println(Thread.currentThread().getName() + "在执行了");
+        // });
+
+        // // Thread.sleep(2000);
+
+        // executorService.submit(() -> {
+        // System.out.println(Thread.currentThread().getName() + "在执行了");
+        // });
+
+        // executorService.shutdown();
+
+
+
+        // // 参数不是初始值而是最大值
+        // ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+        // ThreadPoolExecutor pool = (ThreadPoolExecutor) executorService;
+        // System.out.println(pool.getPoolSize());// 0
+
+        // executorService.submit(() -> {
+        //     System.out.println(Thread.currentThread().getName() + "在执行了");
+        // });
+
+        // executorService.submit(() -> {
+        //     System.out.println(Thread.currentThread().getName() + "在执行了");
+        // });
+
+        // System.out.println(pool.getPoolSize());// 2
+        // // executorService.shutdown();
     }
 }
--- JavaSE/chapter13_Thread_/src/Thread/mythread/a15threadpool/MyThreadPoolDemo1.java
+++ JavaSE/chapter13_Thread_/src/Thread/mythread/a15threadpool/MyThreadPoolDemo1.java
@@ -7,28 +7,27 @@ import java.util.concurrent.TimeUnit;
 
 public class MyThreadPoolDemo1 {
     public static void main(String[] args) {
-
-    /*
-        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor
-        (核心线程数量,最大线程数量,空闲线程最大存活时间,任务队列,创建线程工厂,任务的拒绝策略);
-
-        参数一：核心线程数量              不能小于0
-        参数二：最大线程数                不能小于0，最大数量 >= 核心线程数量
-        参数三：空闲线程最大存活时间       不能小于0
-        参数四：时间单位                  用TimeUnit指定
-        参数五：任务队列                  不能为null
-        参数六：创建线程工厂              不能为null
-        参数七：任务的拒绝策略             不能为null
-    */
+        /*
+         * ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor
+         * (核心线程数量,最大线程数量,空闲线程最大存活时间,任务队列,创建线程工厂,任务的拒绝策略);
+         * 
+         * 参数一：核心线程数量 不能小于0
+         * 参数二：最大线程数 不能小于0，最大数量 >= 核心线程数量
+         * 参数三：空闲线程最大存活时间 不能小于0
+         * 参数四：时间单位 用TimeUnit指定
+         * 参数五：任务队列 不能为null
+         * 参数六：创建线程工厂 不能为null
+         * 参数七：任务的拒绝策略 不能为null
+         */
 
         ThreadPoolExecutor pool = new ThreadPoolExecutor(
-                3,  //核心线程数量，能小于0
-                6,  //最大线程数，不能小于0，最大数量 >= 核心线程数量
-                60,//空闲线程最大存活时间
-                TimeUnit.SECONDS,//时间单位
-                new ArrayBlockingQueue<>(3),//任务队列
-                Executors.defaultThreadFactory(),//创建线程工厂
-                new ThreadPoolExecutor.AbortPolicy()//任务的拒绝策略
+                3, // 核心线程数量，能小于0
+                6, // 最大线程数，不能小于0，最大数量 >= 核心线程数量
+                60, // 空闲线程最大存活时间
+                TimeUnit.SECONDS, // 时间单位
+                new ArrayBlockingQueue<>(3), // 任务队列
+                Executors.defaultThreadFactory(), // 创建线程工厂
+                new ThreadPoolExecutor.AbortPolicy()// 任务的拒绝策略
         );
     }
 }
