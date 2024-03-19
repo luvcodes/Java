9e94a6ce41c1ae59d9626f6bee48118fedb780e2
Runze Yang <ryanyyyyyyy@163.com>
2024-03-19 11:21:43 +1100

    HashMap practice question update

--- JavaSE/chapter11_Collection_/src/Map_8/a02myhashmap/A02_HashMapDemo2.java
+++ JavaSE/chapter11_Collection_/src/Map_8/a02myhashmap/A02_HashMapDemo2.java
@@ -2,6 +2,9 @@ package Map_8.a02myhashmap;
 
 import java.util.*;
 
+/**
+ * @author yangrunze
+ */
 public class A02_HashMapDemo2 {
     public static void main(String[] args) {
         /*
@@ -9,9 +12,11 @@ public class A02_HashMapDemo2 {
             班长提供了四个景点依次是（A、B、C、D）,
             每个学生只能选择一个景点，请统计出最终哪个景点想去的人数最多。
         */
+
         //1.需要先让同学们投票
         //定义一个数组，存储4个景点
-        String[] arr = {"A","B","C","D"};
+        String[] arr = {"A", "B", "C", "D"};
+
         //利用随机数模拟80个同学的投票，并把投票的结果存储起来
         ArrayList<String> list = new ArrayList<>();
         Random r = new Random();
@@ -19,22 +24,23 @@ public class A02_HashMapDemo2 {
             int index = r.nextInt(arr.length);
             list.add(arr[index]);
         }
+
         //2.如果要统计的东西比较多，不方便使用计数器思想
         //我们可以定义map集合，利用集合进行统计。
-        HashMap<String,Integer> hm = new HashMap<>();
+        HashMap<String, Integer> hm = new HashMap<>();
         for (String name : list) {
             //判断当前的景点在map集合当中是否存在
-            if(hm.containsKey(name)){
+            if (hm.containsKey(name)) {
                 //存在
                 //先获取当前景点已经被投票的次数
                 int count = hm.get(name);
                 //表示当前景点又被投了一次
                 count++;
                 //把新的次数再次添加到集合当中
-                hm.put(name,count);
-            }else{
+                hm.put(name, count);
+            } else {
                 //不存在
-                hm.put(name,1);
+                hm.put(name, 1);
             }
         }
 
@@ -45,7 +51,7 @@ public class A02_HashMapDemo2 {
         Set<Map.Entry<String, Integer>> entries = hm.entrySet();
         for (Map.Entry<String, Integer> entry : entries) {
             int count = entry.getValue();
-            if(count > max){
+            if (count > max) {
                 max = count;
             }
         }
@@ -53,11 +59,10 @@ public class A02_HashMapDemo2 {
         //4.判断哪个景点的次数跟最大值一样，如果一样，打印出来
         for (Map.Entry<String, Integer> entry : entries) {
             int count = entry.getValue();
-            if(count == max){
+            if (count == max) {
                 System.out.println(entry.getKey());
             }
         }
-
 
 
     }
