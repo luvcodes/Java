cfb20545fe2699413db5484b609d265932455044
Runze Yang <ryanyyyyyyy@163.com>
2024-03-21 10:03:35 +1100

    Learning HTTP contents

--- PhaseThree/SpringBoot/springboot_01_quickstart1/src/main/java/com/itheima/Springboot01Quickstart1Application.java
+++ PhaseThree/SpringBoot/springboot_01_quickstart1/src/main/java/com/itheima/Springboot01Quickstart1Application.java
@@ -3,6 +3,9 @@ package com.itheima;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author ryanw
+ */
 @SpringBootApplication
 public class Springboot01Quickstart1Application {
     public static void main(String[] args) {
--- PhaseThree/SpringBoot/springboot_01_quickstart1/src/main/java/com/itheima/controller/BookController.java
+++ PhaseThree/SpringBoot/springboot_01_quickstart1/src/main/java/com/itheima/controller/BookController.java
@@ -5,12 +5,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author ryanw
+ */
 @RestController
 @RequestMapping("/books")
 public class BookController {
     @GetMapping("/{id}")
     public String getById(@PathVariable Integer id) {
-        System.out.println("id ==> " + id);
+        System.out.println("id => " + id);
         return "hello springboot!";
     }
 }
