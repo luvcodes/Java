f2b2b792b6ecf37dd147974c136920184dc38813
Runze Yang <ryanyyyyyyy@163.com>
2024-03-21 11:12:29 +1100

    Project import in maven procedure learning

---
+++ PhaseThree/Maven/ryanyang_web_project/maven-project02/src/main/java/org/example/App.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ * @author ryanw
+ */
+public class App {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
