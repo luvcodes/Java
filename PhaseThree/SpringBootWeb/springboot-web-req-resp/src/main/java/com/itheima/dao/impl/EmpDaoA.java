8d3efd8c0a5e423b8105d7e954fe31a3489d9963
Runze Yang <ryanyyyyyyy@163.com>
2024-03-22 17:36:29 +1100

    Response and three layers architecture in springboot web chapter

---
+++ PhaseThree/SpringBootWeb/springboot-web-req-resp/src/main/java/com/itheima/dao/impl/EmpDaoA.java
@@ -0,0 +1,17 @@
+package com.itheima.dao.impl;
+
+import com.itheima.dao.EmpDao;
+import com.itheima.pojo.Emp;
+
+import java.util.List;
+
+/**
+ * @author ryanw
+ */
+public class EmpImpl implements EmpDao {
+
+    @Override
+    public List<Emp> listEmp() {
+
+    }
+}
