a66fd74a69c8a1be3e012f3f961a280adbe31fc8
Runze Yang <ryanyyyyyyy@163.com>
2024-03-25 18:35:56 +1100

    MyBatis contents in searching learning review

--- PhaseThree/MyBatis/springboot-mybatis-crud/src/main/java/com/itheima/mapper/EmpMapper.java
+++ PhaseThree/MyBatis/springboot-mybatis-crud/src/main/java/com/itheima/mapper/EmpMapper.java
@@ -11,26 +11,55 @@ import java.util.List;
  */
 @Mapper
 public interface EmpMapper {
-/*    @Select("select * from emp")
-    public List<Emp> list();*/
-
+    /**
+     * 删除单个emp
+     * */
     @Delete("delete from emp where id = #{id}")
     public void delete(Integer id);
 
+    /**
+     * 插入新emp
+     * */
     // 会自动将生成的主键值，赋值给emp对象的id属性
-    @Options(useGeneratedKeys = true,keyProperty = "id")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
     @Insert("insert into emp (username, name, gender, image, job, entrydate, dept_id, create_time, update_time) " +
-            "values (#{username}, #{name}, #{gender}, #{image}, #{job}, #{entrydate}, #{deptId}, #{createTime}, #{updateTime})")
+            "values (#{username}, #{name}, #{gender}, #{image}, #{job}, #{entrydate}, " +
+            "#{deptId}, #{createTime}, #{updateTime})")
     public void insert(Emp emp);
 
 
     // @Update注解也可以用XML配置文件来替代
-    /*@Update("update emp set username=#{username}, name=#{name}, gender=#{gender}, image=#{image}, job=#{job}, entrydate=#{entrydate}, dept_id=#{deptId}, update_time=#{updateTime} where id=#{id}")*/
+    /**
+     * 根据id修改员工信息
+     */
+    @Update("update emp set username=#{username}, name=#{name}, gender=#{gender}, image=#{image}, job=#{job}, entrydate=#{entrydate}, dept_id=#{deptId}, update_time=#{updateTime} where id=#{id}")
     public void update(Emp emp);
 
+    /**
+     * 根据id查询员工, 需要返回类型
+     * */
+    // 这样写有三个字段封装不上，因为字段名与属性名不一致。
+    /*@Select("select * from emp where id = #{id}")
+    public Eemp getById(Integer id);*/
 
-    // 根据id查询
-    // @Select("select id, username, password, name, gender, image, job, entrydate, dept_id, create_time, update_time from emp where id=#{id}")
+    // 方案一: 给字段起别名，让别名与实体与属性一致
+    // @Select("select id, username, password, name, gender, image, job, entrydate,
+    // dept_id deptId, create_time createTime, update_time updateTime from emp where id=#{id}")
+    // public Emp getById(Integer id);
+
+    // 方案二: 通过@Results、@Result注解手动映射封装
+    /*@Results({
+            @Result(column = "dept_id", property = "deptId"),
+            @Result(column = "create_time", property = "createTime"),
+            @Result(column = "update_time", property = "updateTime")
+    })
+    @Select("select * from emp where id = #{id}")
+    public Emp getById(Integer id);*/
+
+
+    // 方案三: 开启MyBatis的驼峰命名自动映射开关
+    // mybatis.configuration.map-underscore-to-camel-case=true
+    @Select("select * from emp where id = #{id}")
     public Emp getById(Integer id);
 
 
@@ -40,9 +69,12 @@ public interface EmpMapper {
 //            "and gender = #{gender} " +
 //            "and entrydate between #{begin} and #{end} " +
 //            "order by update_time desc")
-    public List<Emp> list(String name, short gender, LocalDate begin, LocalDate end);
+//    public List<Emp> list(String name, short gender, LocalDate begin, LocalDate end);
+
+    /*@Select("select * from emp")
+    public List<Emp> list();*/
 
 
     // 批量删除
-    public void deleteByIds(List<Integer> ids);
+    // public void deleteByIds(List<Integer> ids);
 }
--- PhaseThree/MyBatis/springboot-mybatis-crud/src/test/java/com/itheima/SpringbootMybatisCrudApplicationTests.java
+++ PhaseThree/MyBatis/springboot-mybatis-crud/src/test/java/com/itheima/SpringbootMybatisCrudApplicationTests.java
@@ -15,24 +15,16 @@ class SpringbootMybatisCrudApplicationTests {
     @Autowired
     private EmpMapper empMapper;
 
-/*    @Test
-    void testList() {
-        List<Emp> empList = empMapper.list();
-        for (Emp emp : empList) {
-            System.out.println(emp);
-        }
-    }*/
-
     @Test
     void testDelete() {
-        empMapper.delete(17);
+        empMapper.delete(18);
     }
 
     @Test
     void testInsert() {
         // 创建员工对象
         Emp emp = new Emp();
-        emp.setUsername("tom");
+        emp.setUsername("tom3");
         emp.setName("汤姆");
         emp.setImage("1.jpg");
         emp.setGender((short)1);
@@ -43,14 +35,15 @@ class SpringbootMybatisCrudApplicationTests {
         emp.setDeptId(1);
         empMapper.insert(emp);
 
-        System.out.println(emp.getDeptId());
+        // 这里能够拿到id是因为EmpMapper的Insert方法定义了Options注解，主键返回
+        System.out.println("Emp current id: " + emp.getId());
     }
 
     @Test
     void testUpdate() {
         // 更新员工对象
         Emp emp = new Emp();
-        emp.setId(23);
+        emp.setId(19);
         emp.setUsername("songdaxia");
         emp.setPassword(null);
         emp.setName("老宋");
@@ -71,60 +64,69 @@ class SpringbootMybatisCrudApplicationTests {
         System.out.println(emp);
     }
 
-    @Test
-    public void testGetByMultiCondition() {
-        // 定义查询参数
-        String name = "张无忌";
-        Short gender = 1; // 假设性别用1表示男性
-        LocalDate begin = LocalDate.of(2015, 1, 1);
-        LocalDate end = LocalDate.now(); // 假设查询结束日期为当前日期
 
-        // 调用Mapper接口的list方法
-        List<Emp> result = empMapper.list(name, gender, begin, end);
-
-        // 断言结果不为空
-        assert !result.isEmpty();
-
-        // 打印结果，或进行其他形式的验证
-        result.forEach(emp -> System.out.println(emp.toString()));
-    }
-
-    @Test
-    public void testList(){
-        //只有性别
-        List<Emp> list = empMapper.list(null, (short)1, null, null);
-        for(Emp emp : list){
-            System.out.println(emp);
-        }
-    }
-
-
-    /**
-     * update方法修改成XML配置文件中实现 对应的测试方法
-     * */
-    @Test
-    void testUpdate2() {
-        // 要修改的员工信息
-        Emp emp = new Emp();
-        emp.setId(18);
-        emp.setUsername("Tom111");
-        emp.setName("汤姆111");
-        emp.setUpdateTime(LocalDateTime.now());
-
-        // 调用方法，修改员工数据
-        empMapper.update(emp);
-    }
-
-/*    @Test
-    public void testUpdate2(){
-        //要修改的员工信息
-        Emp emp = new Emp();
-        emp.setId(20);
-        emp.setUsername("Tom222");
-
-        //调用方法，修改员工数据
-        empMapper.update(emp);
-    }*/
+//    /*@Test
+//    public void testGetByMultiCondition() {
+//        // 定义查询参数
+//        String name = "张无忌";
+//        Short gender = 1; // 假设性别用1表示男性
+//        LocalDate begin = LocalDate.of(2015, 1, 1);
+//        LocalDate end = LocalDate.now(); // 假设查询结束日期为当前日期
+//
+//        // 调用Mapper接口的list方法
+//        List<Emp> result = empMapper.list(name, gender, begin, end);
+//
+//        // 断言结果不为空
+//        assert !result.isEmpty();
+//
+//        // 打印结果，或进行其他形式的验证
+//        result.forEach(emp -> System.out.println(emp.toString()));
+//    }*/
+//
+//    /*@Test
+//    public void testList(){
+//        //只有性别
+//        List<Emp> list = empMapper.list(null, (short)1, null, null);
+//        for(Emp emp : list){
+//            System.out.println(emp);
+//        }
+//    }*/
+//
+//
+//    /*@Test
+//    void testList() {
+//        List<Emp> empList = empMapper.list();
+//        for (Emp emp : empList) {
+//            System.out.println(emp);
+//        }
+//    }*/
+//
+//    /**
+//     * update方法修改成XML配置文件中实现 对应的测试方法
+//     * */
+//    @Test
+//    void testUpdate2() {
+//        // 要修改的员工信息
+//        Emp emp = new Emp();
+//        emp.setId(18);
+//        emp.setUsername("Tom111");
+//        emp.setName("汤姆111");
+//        emp.setUpdateTime(LocalDateTime.now());
+//
+//        // 调用方法，修改员工数据
+//        empMapper.update(emp);
+//    }
+//
+///*    @Test
+//    public void testUpdate2(){
+//        //要修改的员工信息
+//        Emp emp = new Emp();
+//        emp.setId(20);
+//        emp.setUsername("Tom222");
+//
+//        //调用方法，修改员工数据
+//        empMapper.update(emp);
+//    }*/
 
 
 }
