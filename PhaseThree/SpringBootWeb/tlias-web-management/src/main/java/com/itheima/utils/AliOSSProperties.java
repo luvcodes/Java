66acd83837aaf5e4f13dc4d43828057d4be68515
Runze Yang <ryanyyyyyyy@163.com>
2024-03-31 09:05:03 +1100

    Update on the AliOSS functionality configuration contents

--- PhaseThree/SpringBootWeb/tlias-web-management/src/main/java/com/itheima/utils/AliOSSProperties.java
+++ PhaseThree/SpringBootWeb/tlias-web-management/src/main/java/com/itheima/utils/AliOSSProperties.java
@@ -4,6 +4,9 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+/**
+ * @author ryanw
+ */
 @Data
 @Component
 @ConfigurationProperties(prefix = "aliyun.oss")
