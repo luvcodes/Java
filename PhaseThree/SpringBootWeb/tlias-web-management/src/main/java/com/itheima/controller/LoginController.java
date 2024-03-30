ee5d2b9f5c8e31b1d5b354c85c4b489ff19ba4d3
Runze Yang <ryanyyyyyyy@163.com>
2024-03-31 10:07:37 +1100

    JWT login functionality implementation complete

--- PhaseThree/SpringBootWeb/tlias-web-management/src/main/java/com/itheima/controller/LoginController.java
+++ PhaseThree/SpringBootWeb/tlias-web-management/src/main/java/com/itheima/controller/LoginController.java
@@ -3,11 +3,14 @@ package com.itheima.controller;
 import com.itheima.pojo.Emp;
 import com.itheima.pojo.Result;
 import com.itheima.service.EmpService;
+import com.itheima.utils.JwtUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
 
 /**
  * @author ryanw
@@ -21,8 +24,20 @@ public class LoginController {
     @PostMapping("/login")
     public Result login(@RequestBody Emp emp) {
         log.info("用户登录 {}", emp);
-        // 登录就是校验用户，密码的有效性
+        // 调用业务层：登录功能
         Emp e = empService.login(emp);
-        return e != null ? Result.success() : Result.error("用户名或密码错误");
+        // 判断: 登录用户是否存在
+        if (e != null) {
+            // 自定义信息
+            HashMap<String, Object> claims = new HashMap<>();
+            claims.put("id", e.getId());
+            claims.put("username", e.getUsername());
+            claims.put("name", e.getName());
+
+            // 使用JWT工具类，生成身份令牌
+            String token = JwtUtils.generateJwt(claims);
+            return Result.success(token);
+        }
+        return Result.error("用户名或密码错误");
     }
 }
--- PhaseThree/SpringBootWeb/tlias-web-management/src/main/java/com/itheima/pojo/Result.java
+++ PhaseThree/SpringBootWeb/tlias-web-management/src/main/java/com/itheima/pojo/Result.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * @author ryanw
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -16,15 +19,17 @@ public class Result {
     private Object data;
 
     //增删改 成功响应
-    public static Result success(){
-        return new Result(1,"success",null);
+    public static Result success() {
+        return new Result(1, "success", null);
     }
+
     //查询 成功响应
-    public static Result success(Object data){
-        return new Result(1,"success",data);
+    public static Result success(Object data) {
+        return new Result(1, "success", data);
     }
+
     //失败响应
-    public static Result error(String msg){
-        return new Result(0,msg,null);
+    public static Result error(String msg) {
+        return new Result(0, msg, null);
     }
 }
