428f42daef09eccbdd1ab3984ff874abb64d8133
Runze Yang <ryanyyyyyyy@163.com>
2024-04-02 11:15:10 +1100

    Annotation in AOP

--- PhaseThree/Spring/springboot-aop-quickstart/src/main/java/com/itheima/aop/MyLog.java
+++ PhaseThree/Spring/springboot-aop-quickstart/src/main/java/com/itheima/aop/MyLog.java
@@ -6,7 +6,10 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * 自定义注解，体现annotation注解
  * @author ryanw
+ * Retention是为了体现什么时候生效的
+ * Target是用来指定注解可以作用在哪些地方
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
