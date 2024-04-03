ceffed0b71eb075d540a3ce505d2a4343f264651
Runze Yang <ryanyyyyyyy@163.com>
2024-04-04 01:10:52 +1100

    Update on the project class files

--- PhaseThree/SpringBootWeb/tlias-web-management/src/main/java/com/itheima/TliasWebManagementApplication.java
+++ PhaseThree/SpringBootWeb/tlias-web-management/src/main/java/com/itheima/TliasWebManagementApplication.java
@@ -4,12 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
+/**
+ * @author yangrunze
+ */
 @ServletComponentScan //开启了对servlet组件的支持
 @SpringBootApplication
 public class TliasWebManagementApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TliasWebManagementApplication.class, args);
     }
-
 }
