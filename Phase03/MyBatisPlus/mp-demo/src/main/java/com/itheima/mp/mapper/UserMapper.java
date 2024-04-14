3af9c28c0706d59443efc29324497e17d5d4ae6d
Runze Yang <ryanyyyyyyy@163.com>
2024-04-14 16:35:44 +1000

    update on the first chapter content

---
+++ Phase03/MyBatisPlus/mp-demo/src/main/java/com/itheima/mp/mapper/UserMapper.java
@@ -0,0 +1,24 @@
+package com.itheima.mp.mapper;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.toolkit.Constants;
+import com.itheima.mp.domain.po.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+/**
+ * @author ryanw
+ */
+@Mapper
+public interface UserMapper extends BaseMapper<User> {
+    List<User> queryUserByIds(@Param("ids") List<Long> ids);
+
+    void updateBalanceByIds(@Param(Constants.WRAPPER) QueryWrapper<User> wrapper, @Param("amount") int amount);
+
+    @Update("UPDATE user SET balance = balance - #{money} WHERE id = #{id}")
+    void deductBalance(@Param("id") Long id, @Param("money")Integer money);
+}
---
+++ Phase04/SpringCloud01/cloud-demo/order-service/src/main/java/cn/itcast/order/OrderApplication.java
@@ -0,0 +1,26 @@
+package cn.itcast.order;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author ryanw
+ */
+@MapperScan("cn.itcast.order.mapper")
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+}
