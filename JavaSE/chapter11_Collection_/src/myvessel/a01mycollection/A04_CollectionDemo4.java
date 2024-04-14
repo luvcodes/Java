a3dfa2907906b64f4cc3e29d9049d1d15e7511f8
Runze Yang <ryanyyyyyyy@163.com>
2024-04-14 23:55:06 +1000

    Update on the collection class contents

---
+++ JavaSE/chapter11_Collection_/src/myvessel/a01mycollection/A04_CollectionDemo4.java
@@ -0,0 +1,18 @@
+package myvessel.a01mycollection;
+
+/**
+ * @author yangrunze
+ */
+public class A04_CollectionDemo4 {
+    public static void main(String[] args) {
+      /*
+        迭代器的细节注意点：
+            1.报错NoSuchElementException
+            2.迭代器遍历完毕，指针不会复位
+            3.循环中只能用一次next方法
+            4.迭代器遍历时，不能用集合的方法进行增加或者删除
+       */
+
+
+    }
+}
---
+++ JavaSE/chapter11_Collection_/src/myvessel/a01mycollection/Student.java
@@ -0,0 +1,72 @@
+package myvessel.a01mycollection;
+
+import java.util.Objects;
+
+/**
+ * @author yangrunze
+ */
+public class Student {
+    private String name;
+    private int age;
+
+
+    public Student() {
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    /**
+     * 获取
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 设置
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 获取
+     * @return age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * 设置
+     * @param age
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Student student = (Student) o;
+        return age == student.age && Objects.equals(name, student.name);
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Student{name = " + name + ", age = " + age + "}";
+    }
+}
