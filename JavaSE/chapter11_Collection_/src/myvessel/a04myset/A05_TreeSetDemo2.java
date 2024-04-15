2f03f7325d13f748bcd5d3b1267799881db6b905
Runze Yang <ryanyyyyyyy@163.com>
2024-04-15 10:32:56 +1000

    compareTo method in TreeSet

--- JavaSE/chapter11_Collection_/src/myvessel/a04myset/A05_TreeSetDemo2.java
+++ JavaSE/chapter11_Collection_/src/myvessel/a04myset/A05_TreeSetDemo2.java
@@ -1,6 +1,6 @@
 package myvessel.a04myset;
 
-
+import java.util.Iterator;
 import java.util.TreeSet;
 
 /**
@@ -9,23 +9,22 @@ import java.util.TreeSet;
 public class A05_TreeSetDemo2 {
     public static void main(String[] args) {
         /*
-            需求：创建TreeSet集合，并添加3个学生对象
-            学生对象属性：
-                姓名，年龄。
-                要求按照学生的年龄进行排序
-                同年龄按照姓名字母排列（暂不考虑中文）
-                同姓名，同年龄认为是同一个人
-
-            方式一：
-                默认的排序规则/自然排序
-                Student实现Comparable接口，重写里面的抽象方法，再指定比较规则
-        */
+         * 需求：创建TreeSet集合，并添加3个学生对象
+         * 学生对象属性：姓名，年龄。
+         * 要求按照学生的年龄进行排序
+         * 同年龄按照姓名字母排列（暂不考虑中文）
+         * 同姓名，同年龄认为是同一个人
+         * 
+         * 方式一：
+         * 默认的排序规则/自然排序
+         * Student实现Comparable接口，重写里面的抽象方法，再指定比较规则
+         */
 
         // 1. 创建三个学生对象
-        Student s1 = new Student("zhangsan",23);
-        Student s2 = new Student("lisi",24);
-        Student s3 = new Student("wangwu",25);
-        Student s4 = new Student("zhaoliu",26);
+        Student s1 = new Student("zhangsan", 23);
+        Student s2 = new Student("lisi", 24);
+        Student s3 = new Student("wangwu", 25);
+        Student s4 = new Student("zhaoliu", 26);
 
         // 2. 创建集合对象
         TreeSet<Student> ts = new TreeSet<>();
@@ -36,8 +35,16 @@ public class A05_TreeSetDemo2 {
         ts.add(s1);
         ts.add(s4);
 
-        //4.打印集合
+        // 4.打印集合
         System.out.println("最终结果");
         System.out.println(ts);
+
+        // 如果是比较基本数据类型，就不需要重写compareTo方法
+        // TreeSet<String> ts2 = new TreeSet<>();
+        // ts2.add("abc");
+        // ts2.add("def");
+        // ts2.add("ghi");
+        // ts2.add("bcd");
+        // System.out.println(ts2);
     }
 }
