51f37b05009ab727e87ce81c89e3c65cf1d21356
Runze Yang <ryanyyyyyyy@163.com>
2024-05-04 18:22:16 +1000

    Update on the controller code comments

--- Phase04/Redis/commentApp/hm-dianping/src/main/java/com/hmdp/controller/BlogCommentsController.java
+++ Phase04/Redis/commentApp/hm-dianping/src/main/java/com/hmdp/controller/BlogCommentsController.java
@@ -6,12 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * <p>
  *  前端控制器
- * </p>
- *
- * @author 虎哥
- * @since 2021-12-22
+ * @author yangrunze
  */
 @RestController
 @RequestMapping("/blog-comments")
