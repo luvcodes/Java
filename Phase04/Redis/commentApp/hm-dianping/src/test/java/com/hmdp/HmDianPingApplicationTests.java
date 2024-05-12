ea30a8caa50fad7f6bd3e4ecb727e6cb41263b3d
Runze Yang <ryanyyyyyyy@163.com>
2024-05-12 11:39:55 +1000

    点评项目导入店铺数据到Redis GEO

--- Phase04/Redis/commentApp/hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ Phase04/Redis/commentApp/hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,20 +1,42 @@
 package com.hmdp;
 
+import com.hmdp.entity.Shop;
+import com.hmdp.service.IShopService;
+import com.hmdp.utils.CacheClient;
+import com.hmdp.utils.RedisConstants;
 import com.hmdp.utils.RedisIdWorker;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.geo.Point;
+import org.springframework.data.redis.connection.RedisGeoCommands;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 @SpringBootTest
-class HmDianPingApplicationTests {
+public class HmDianPingApplicationTests {
     @Resource
     private RedisIdWorker redisIdWorker;
 
     private ExecutorService es = Executors.newFixedThreadPool(500);
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Resource
+    private IShopService shopService;
+    @Autowired
+    private CacheClient cacheClient;
 
     /*@Test
     void testIdWorker throws InterruptedException() {
@@ -32,5 +54,41 @@ class HmDianPingApplicationTests {
         long end = System.currentTimeMillis();
     }*/
 
+    @Test
+    public void testSaveShop() {
+        Shop shop = shopService.getById(1L);
+        cacheClient.setWithLogicalExpire(RedisConstants.CACHE_SHOP_KEY + 1L, shop, 10L, TimeUnit.SECONDS);
+    }
 
+
+    @Test
+    public void loadShopData() {
+        // 1. 查询店铺信息
+        List<Shop> list = shopService.list();
+
+        // 2. 把店铺按照typeId进行分组，typeId一致的放到同一个集合中
+        Map<Long, List<Shop>> map = list.stream().collect(Collectors.groupingBy(shop -> shop.getTypeId()));
+
+        // 3. 分批完成分组写入集合，进行存储
+        for (Map.Entry<Long, List<Shop>> entry : map.entrySet()) {
+            // 3.1 获取类型id
+            Long typeId = entry.getKey();
+            String key = "shop:geo:" + typeId;
+            // 3.2 获取同类型的店铺集合
+            List<Shop> shops = entry.getValue();
+            List<RedisGeoCommands.GeoLocation<String>> locations = new ArrayList<>(shops.size());
+
+            // 3.3 写入redis GEOADD key 经度 维度 member
+            for (Shop shop : shops) {
+                // stringRedisTemplate.opsForGeo().add(key, new Point(shop.getX(), shop.getY()), shop.getId().toString());
+                locations.add(new RedisGeoCommands.GeoLocation<>(
+                        shop.getId().toString(),
+                        new Point(shop.getX(), shop.getY())
+                ));
+            }
+
+            // 3.4 批量写到redis中
+            stringRedisTemplate.opsForGeo().add(key, locations);
+        }
+    }
 }
