f9974574946fbe155ec58888e170a5f142172933
Runze Yang <ryanyyyyyyy@163.com>
2024-05-17 10:29:02 +1000

    Redis高级 - JVM进程缓存 Caffeine basics APIs

--- Phase04/Redis/RedisChapter3/item-service/src/test/java/com/heima/item/test/CaffeineTest.java
+++ Phase04/Redis/RedisChapter3/item-service/src/test/java/com/heima/item/test/CaffeineTest.java
@@ -7,10 +7,6 @@ import org.junit.jupiter.api.Test;
 import java.time.Duration;
 
 public class CaffeineTest {
-
-    /*
-      基本用法测试
-     */
     @Test
     void testBasicOps() {
         // 创建缓存对象
@@ -31,8 +27,8 @@ public class CaffeineTest {
         System.out.println("defaultGF = " + defaultGF);
     }
 
-    /*
-     基于大小设置驱逐策略：
+    /**
+     * 基于大小设置驱逐策略, 也就是清理缓存
      */
     @Test
     void testEvictByNum() throws InterruptedException {
@@ -41,20 +37,23 @@ public class CaffeineTest {
                 // 设置缓存大小上限为 1
                 .maximumSize(1)
                 .build();
+
         // 存数据
         cache.put("gf1", "柳岩");
         cache.put("gf2", "范冰冰");
         cache.put("gf3", "迪丽热巴");
+
         // 延迟10ms，给清理线程一点时间
         Thread.sleep(10L);
+
         // 获取数据
         System.out.println("gf1: " + cache.getIfPresent("gf1"));
         System.out.println("gf2: " + cache.getIfPresent("gf2"));
         System.out.println("gf3: " + cache.getIfPresent("gf3"));
     }
 
-    /*
-     基于时间设置驱逐策略：
+    /**
+     * 基于时间设置驱逐策略, 也就是清理缓存
      */
     @Test
     void testEvictByTime() throws InterruptedException {
@@ -62,10 +61,13 @@ public class CaffeineTest {
         Cache<String, String> cache = Caffeine.newBuilder()
                 .expireAfterWrite(Duration.ofSeconds(1)) // 设置缓存有效期为 10 秒
                 .build();
+
         // 存数据
         cache.put("gf", "柳岩");
+
         // 获取数据
         System.out.println("gf: " + cache.getIfPresent("gf"));
+
         // 休眠一会儿
         Thread.sleep(1200L);
         System.out.println("gf: " + cache.getIfPresent("gf"));
