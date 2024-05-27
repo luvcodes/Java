0328c5b6e316e5c0e85499bf7e4510cf62766488
Runze Yang <ryanyyyyyyy@163.com>
2024-05-27 23:19:49 +1000

    2024 micro service project import

---
+++ Phase04/MicroServiceNew/MicroService/hmall/item-service/src/main/java/com/hmall/item/ItemApplication.java
@@ -0,0 +1,16 @@
+package com.hmall.item;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ryanw
+ */
+@MapperScan("com.hmall.item.mapper")
+@SpringBootApplication
+public class ItemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ItemApplication.class, args);
+    }
+}
---
+++ Phase04/MicroServiceNew/MicroService/hmall/item-service/src/main/java/com/hmall/item/mapper/ItemMapper.java
@@ -0,0 +1,16 @@
+package com.hmall.item.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hmall.item.domain.dto.OrderDetailDTO;
+import com.hmall.item.domain.po.Item;
+import org.apache.ibatis.annotations.Update;
+
+/**
+ * 商品表 Mapper 接口
+ * @author ryanw
+ */
+public interface ItemMapper extends BaseMapper<Item> {
+
+    @Update("UPDATE item SET stock = stock - #{num} WHERE id = #{itemId}")
+    void updateStock(OrderDetailDTO orderDetail);
+}
