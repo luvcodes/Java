7be1395e32ad0d027c17211e0f61dec10050708d
Runze Yang <ryanyyyyyyy@163.com>
2024-05-28 11:54:36 +1000

    MicroService remote produce call, RPC远程调用通过RestTemplate

---
+++ Phase04/MicroServiceNew/MicroService/hmall/cart-service/src/main/java/com/hmall/cart/CartApplication.java
@@ -0,0 +1,23 @@
+package com.hmall.cart;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author ryanw
+ */
+@MapperScan("com.hmall.cart.mapper")
+@SpringBootApplication
+public class CartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CartApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
---
+++ Phase04/MicroServiceNew/MicroService/hmall/cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
@@ -0,0 +1,149 @@
+package com.hmall.cart.service.impl;
+
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmall.cart.domain.dto.CartFormDTO;
+import com.hmall.cart.domain.dto.ItemDTO;
+import com.hmall.cart.domain.po.Cart;
+import com.hmall.cart.domain.vo.CartVO;
+import com.hmall.cart.mapper.CartMapper;
+import com.hmall.cart.service.ICartService;
+import com.hmall.common.exception.BizIllegalException;
+import com.hmall.common.utils.BeanUtils;
+import com.hmall.common.utils.CollUtils;
+import com.hmall.common.utils.UserContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * 订单详情表 服务实现类
+ * @author ryanw
+ */
+@Service
+@RequiredArgsConstructor
+public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements ICartService {
+
+    // 不使用自动注入，使用构造函数的方式
+    // final 关键字确保了依赖在对象构造完成后不会被改变，从而增强了对象的稳定性和线程安全性
+    private final RestTemplate restTemplate;
+
+    @Override
+    public void addItem2Cart(CartFormDTO cartFormDTO) {
+        // 1.获取登录用户
+        Long userId = UserContext.getUser();
+
+        // 2.判断是否已经存在
+        if(checkItemExists(cartFormDTO.getItemId(), userId)){
+            // 2.1.存在，则更新数量
+            baseMapper.updateNum(cartFormDTO.getItemId(), userId);
+            return;
+        }
+        // 2.2.不存在，判断是否超过购物车数量
+        checkCartsFull(userId);
+
+        // 3.新增购物车条目
+        // 3.1.转换PO
+        Cart cart = BeanUtils.copyBean(cartFormDTO, Cart.class);
+        // 3.2.保存当前用户
+        cart.setUserId(userId);
+        // 3.3.保存到数据库
+        save(cart);
+    }
+
+    @Override
+    public List<CartVO> queryMyCarts() {
+        // 1.查询我的购物车列表
+        List<Cart> carts = lambdaQuery().eq(Cart::getUserId, 1L /*UserContext.getUser()*/).list();
+        if (CollUtils.isEmpty(carts)) {
+            return CollUtils.emptyList();
+        }
+
+        // 2.转换VO
+        List<CartVO> vos = BeanUtils.copyList(carts, CartVO.class);
+
+        // 3.处理VO中的商品信息
+        handleCartItems(vos);
+
+        // 4.返回
+        return vos;
+    }
+
+    private void handleCartItems(List<CartVO> vos) {
+        // TODO 获取商品数据
+        // 1.获取商品id
+        Set<Long> itemIds = vos.stream().map(CartVO::getItemId).collect(Collectors.toSet());
+        // 2.查询商品
+        // List<ItemDTO> items = itemService.queryItemByIds(itemIds);
+        // 2.1 利用RestTemplate发起http请求，得到http的响应
+        // 创建一个HashMap实例
+        Map<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("ids", CollUtil.join(itemIds, ","));
+
+        ResponseEntity<List<ItemDTO>> response = restTemplate.exchange(
+                "http://localhost:8081/items?ids={ids}",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<ItemDTO>>() {},
+                uriVariables
+        );
+
+        // 2.2 解析响应
+        if (!response.getStatusCode().is2xxSuccessful()) {
+            // 查询失败，直接结束
+            return;
+        }
+
+        List<ItemDTO> items = response.getBody();
+
+        if (CollUtils.isEmpty(items)) {
+            return;
+        }
+        // 3.转为 id 到 item的map
+        Map<Long, ItemDTO> itemMap = items.stream().collect(Collectors.toMap(ItemDTO::getId, Function.identity()));
+        // 4.写入vo
+        for (CartVO v : vos) {
+            ItemDTO item = itemMap.get(v.getItemId());
+            if (item == null) {
+                continue;
+            }
+            v.setNewPrice(item.getPrice());
+            v.setStatus(item.getStatus());
+            v.setStock(item.getStock());
+        }
+    }
+
+    @Override
+    public void removeByItemIds(Collection<Long> itemIds) {
+        // 1.构建删除条件，userId和itemId
+        QueryWrapper<Cart> queryWrapper = new QueryWrapper<Cart>();
+        queryWrapper.lambda()
+                .eq(Cart::getUserId, UserContext.getUser())
+                .in(Cart::getItemId, itemIds);
+        // 2.删除
+        remove(queryWrapper);
+    }
+
+    private void checkCartsFull(Long userId) {
+        int count = lambdaQuery().eq(Cart::getUserId, userId).count();
+        if (count >= 10) {
+            throw new BizIllegalException(StrUtil.format("用户购物车课程不能超过{}", 10));
+        }
+    }
+
+    private boolean checkItemExists(Long itemId, Long userId) {
+        int count = lambdaQuery()
+                .eq(Cart::getUserId, userId)
+                .eq(Cart::getItemId, itemId)
+                .count();
+        return count > 0;
+    }
+}
