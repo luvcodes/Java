8e248a92241eea41d225258dcf86e3778928efe8
Runze Yang <ryanyyyyyyy@163.com>
2024-05-29 16:50:20 +1000

    MicroService实现注册中心、DiscoveryClient实现服务发现

--- Phase04/MicroServiceNew/MicroService/hmall/cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
+++ Phase04/MicroServiceNew/MicroService/hmall/cart-service/src/main/java/com/hmall/cart/service/impl/CartServiceImpl.java
@@ -1,6 +1,7 @@
 package com.hmall.cart.service.impl;
 
 import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.util.RandomUtil;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -15,11 +16,15 @@ import com.hmall.common.utils.BeanUtils;
 import com.hmall.common.utils.CollUtils;
 import com.hmall.common.utils.UserContext;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
 import java.util.*;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -35,6 +40,9 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
     // 不使用自动注入，使用构造函数的方式
     // final 关键字确保了依赖在对象构造完成后不会被改变，从而增强了对象的稳定性和线程安全性
     private final RestTemplate restTemplate;
+
+    // 服务发现, SpringCloud内置
+    private final DiscoveryClient discoveryClient;
 
     @Override
     public void addItem2Cart(CartFormDTO cartFormDTO) {
@@ -83,13 +91,21 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
         Set<Long> itemIds = vos.stream().map(CartVO::getItemId).collect(Collectors.toSet());
         // 2.查询商品
         // List<ItemDTO> items = itemService.queryItemByIds(itemIds);
-        // 2.1 利用RestTemplate发起http请求，得到http的响应
-        // 创建一个HashMap实例
+        // 2.1 根据服务名称获取示例列表
+        List<ServiceInstance> instances = discoveryClient.getInstances("item-service");
+        if (CollUtil.isEmpty(instances)) {
+            return;
+        }
+        // 2.2 负载均衡，从示例列表中挑选一个
+        ServiceInstance instance = instances.get(RandomUtil.randomInt(instances.size()));
+        // 获取实例的IP和端口
+        URI uri = instance.getUri();
+        // 2.3 利用RestTemplate发起http请求，得到http的响应
         Map<String, String> uriVariables = new HashMap<>();
         uriVariables.put("ids", CollUtil.join(itemIds, ","));
 
         ResponseEntity<List<ItemDTO>> response = restTemplate.exchange(
-                "http://localhost:8081/items?ids={ids}",
+                uri + "/items?ids={ids}",
                 HttpMethod.GET,
                 null,
                 new ParameterizedTypeReference<List<ItemDTO>>() {},
