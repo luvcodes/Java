d8dd0a848b90d0e8db55b2799e26e3a868bfce9f
Runze Yang <ryanyyyyyyy@163.com>
2024-06-27 23:49:02 +1000

    Update on the comments of the service layer

--- Phase04/MicroServiceNew/MicroService/hmall/hm-service/src/main/java/com/hmall/service/IAddressService.java
+++ Phase04/MicroServiceNew/MicroService/hmall/hm-service/src/main/java/com/hmall/service/IAddressService.java
@@ -4,12 +4,8 @@ import com.hmall.domain.po.Address;
 import com.baomidou.mybatisplus.extension.service.IService;
 
 /**
- * <p>
  *  服务类
- * </p>
- *
- * @author 虎哥
- * @since 2023-05-05
+ * @author ryanw
  */
 public interface IAddressService extends IService<Address> {
 
--- Phase04/MicroServiceNew/MicroService/hmall/hm-service/src/main/java/com/hmall/service/ICartService.java
+++ Phase04/MicroServiceNew/MicroService/hmall/hm-service/src/main/java/com/hmall/service/ICartService.java
@@ -9,12 +9,8 @@ import java.util.Collection;
 import java.util.List;
 
 /**
- * <p>
  * 订单详情表 服务类
- * </p>
- *
- * @author 虎哥
- * @since 2023-05-05
+ * @author ryanw
  */
 public interface ICartService extends IService<Cart> {
 
