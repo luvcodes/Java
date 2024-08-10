074ed8b9b85e380d0717c5ed56c1ef8b27a89970
Runze Yang <ryanyyyyyyy@163.com>
2024-08-10 15:36:37 +0800

    MQ chapter learning

---
+++ Phase04/MQ/mq-demo/consumer/src/main/java/com/itheima/consumer/ConsumerApplication.java
@@ -0,0 +1,14 @@
+package com.itheima.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ryanw
+ */
+@SpringBootApplication
+public class ConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+}
---
+++ Phase04/MQ/mq-demo/publisher/src/main/java/com/itheima/publisher/PublisherApplication.java
@@ -0,0 +1,14 @@
+package com.itheima.publisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ryanw
+ */
+@SpringBootApplication
+public class PublisherApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PublisherApplication.class);
+    }
+}
