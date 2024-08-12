af486395ad64473882986f9e4df469b50457b320
Runze Yang <ryanyyyyyyy@163.com>
2024-08-12 13:33:06 +0800

    MQ chapter - WorkQueues model content experiment

--- Phase04/MQ/mq-demo/consumer/src/main/java/com/itheima/consumer/mq/SpringRabbitListener.java
+++ Phase04/MQ/mq-demo/consumer/src/main/java/com/itheima/consumer/mq/SpringRabbitListener.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
 /**
  * @author ryanw
  */
@@ -16,6 +18,19 @@ public class SpringRabbitListener {
     public void listenSimpleQueue(String message) {
         // 处理业务
         log.info("监听到simple.queue的消息: ${}", message);
+    }
+
+    // 消费者1
+    @RabbitListener(queues = "work.queue")
+    public void listenWorkQueue1(String message) {
+        System.out.println("消费者1接收到消息：" + message + ", " + LocalDateTime.now());
+    }
+
+    // 消费者2
+    @RabbitListener(queues = "work.queue")
+    public void listenWorkQueue2(String message) {
+        // 打印出来是红的
+        System.err.println("消费者2接收到消息：" + message + ", " + LocalDateTime.now());
     }
 
 }
--- Phase04/MQ/mq-demo/publisher/src/test/java/com/itheima/publisher/SpringAmqpTest.java
+++ Phase04/MQ/mq-demo/publisher/src/test/java/com/itheima/publisher/SpringAmqpTest.java
@@ -13,14 +13,26 @@ public class SpringAmqpTest {
 
     @Test
     public void testSimpleQueue() {
-
         // 1. 队列名
         String queueName = "simple.queue";
-
         // 2. 消息内容
         String message = "hello, mq";
-
         // 3. 发送消息
         rabbitTemplate.convertAndSend(queueName, message);
+    }
+
+    /**
+     * 发送者 对应消费者1、2 的 workqueue
+     * */
+    @Test
+    public void testWorkQueue() {
+        // 1. 队列名
+        String queueName = "work.queue";
+        for (int i = 0; i <= 50; i++) {
+            // 2. 消息
+            String message = "hello, spring amqp_" + i;
+            // 3. 发送消息
+            rabbitTemplate.convertAndSend(queueName, message);
+        }
     }
 }
