7e223f0333edc85362919af0e22b64704085160d
Runze Yang <ryanyyyyyyy@163.com>
2024-08-17 10:58:46 +0800

    MQ chapter - Declare queue and exchange for the Direct and Topic exchange

--- Phase04/MQ/mq-demo/consumer/src/main/java/com/itheima/consumer/config/DirectConfiguration.java
+++ Phase04/MQ/mq-demo/consumer/src/main/java/com/itheima/consumer/config/DirectConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.amqp.core.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+// @Configuration
 public class DirectConfiguration {
 
     /**
--- Phase04/MQ/mq-demo/consumer/src/main/java/com/itheima/consumer/mq/SpringRabbitListener.java
+++ Phase04/MQ/mq-demo/consumer/src/main/java/com/itheima/consumer/mq/SpringRabbitListener.java
@@ -1,6 +1,10 @@
 package com.itheima.consumer.mq;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
@@ -58,13 +62,23 @@ public class SpringRabbitListener {
      * 因此，在SpringAmqpTest测试类里指定routing key为red，这里两个消费者都能收到
      * */
     // direct消费者1
-    @RabbitListener(queues = "direct.queue1")
+    // @RabbitListener(queues = "direct.queue1")
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(name = "direct.queue1", durable = "true"),
+            exchange = @Exchange(name = "hmall.direct", type = ExchangeTypes.DIRECT),
+            key = {"red", "blue"}
+    ))
     public void listenDirectQueue1(String msg) {
         System.out.println("消费者1接收到direct.queue1的消息：【" + msg + "】");
     }
 
     // direct消费者2
-    @RabbitListener(queues = "direct.queue2")
+    // @RabbitListener(queues = "direct.queue2")
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(name = "direct.queue2", durable = "true"),
+            exchange = @Exchange(name = "hmall.direct", type = ExchangeTypes.DIRECT),
+            key = {"red", "yellow"}
+    ))
     public void listenDirectQueue2(String msg) {
         System.out.println("消费者2接收到direct.queue2的消息：【" + msg + "】");
     }
@@ -72,13 +86,24 @@ public class SpringRabbitListener {
     /**
      * Topic交换机：Direct交换机的升级版，可以指定通配符
      * */
-    @RabbitListener(queues = "topic.queue1")
-    public void listenTopicQueue1(String msg) {
-        System.out.println("消费者1接收到的topic.queue1的消息：[" + msg + "]");
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(name = "topic.queue1"),
+            exchange = @Exchange(name = "hmall.topic", type = ExchangeTypes.TOPIC),
+            key = "china.#"
+    ))
+    // @RabbitListener(queues = "topic.queue1")
+    public void listenTopicQueue1(String msg){
+        System.out.println("消费者1接收到topic.queue1的消息：【" + msg + "】");
     }
 
-    @RabbitListener(queues = "topic.queue2")
-    public void listenTopicQueue2(String msg) {
-        System.out.println("消费者2接收到的topic.queue2的消息：[" + msg + "]");
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(name = "topic.queue2"),
+            exchange = @Exchange(name = "hmall.topic", type = ExchangeTypes.TOPIC),
+            key = "#.news"
+    ))
+    // @RabbitListener(queues = "topic.queue2")
+    public void listenTopicQueue2(String msg){
+        System.out.println("消费者2接收到topic.queue2的消息：【" + msg + "】");
     }
+
 }
