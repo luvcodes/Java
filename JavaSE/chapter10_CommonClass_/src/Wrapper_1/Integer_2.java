4daf00c95f9d3f49f43351304e924d3c43983284
Runze Yang <ryanyyyyyyy@163.com>
2024-09-14 08:51:58 +0800

    Wrapper class of Integer

--- JavaSE/chapter10_CommonClass_/src/Wrapper_1/Integer_2.java
+++ JavaSE/chapter10_CommonClass_/src/Wrapper_1/Integer_2.java
@@ -1,5 +1,9 @@
 package Wrapper_1;
 
+/**
+ * @author ryanw
+ * */
+@SuppressWarnings("all")
 public class Integer_2 {
     public static void main(String[] args) {
         // int <-> Integer 的装箱和拆箱
@@ -8,6 +12,7 @@ public class Integer_2 {
         int n1 = 100;
         Integer integer = new Integer(n1);
         Integer integer1 = Integer.valueOf(n1);
+
         // 手动拆箱 Integer -> int
         int i = integer.intValue();
 
@@ -15,7 +20,23 @@ public class Integer_2 {
         // 自动装箱 int -> Integer
         int n2 = 200;
         Integer integer2 = n2; // 底层使用的是 Integer.valueOf(n2)
+
         // 自动拆箱 Integer -> int
         int n3 = integer2; // 底层使用的是intValue() 方法
+    }
+
+    /**
+     * 展示Integer的实例化方式
+     * */
+    public static void aboutInteger() {
+        // 1. 通过new关键字来进行实例化
+        Integer first = new Integer(100);
+
+        // 2. 通过valueOf方法进行实例化
+        Integer second = Integer.valueOf(200);
+
+        // 3. 使用字符串构造函数
+        String third = "123";
+        String thirdInt = String.valueOf(third);
     }
 }
