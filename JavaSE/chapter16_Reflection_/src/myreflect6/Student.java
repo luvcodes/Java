1de10c80df4eee16379f632dd59fca43791e673d
Runze Yang <ryanyyyyyyy@163.com>
2024-09-20 23:12:18 +0800

    Update on the Reflection chapter object class

--- JavaSE/chapter16_Reflection_/src/myreflect6/Student.java
+++ JavaSE/chapter16_Reflection_/src/myreflect6/Student.java
@@ -7,8 +7,7 @@ public class Student {
     private String name;
     private int age;
 
-    public Student() {
-    }
+    public Student() {}
 
     public Student(String name, int age) {
         this.name = name;
@@ -19,34 +18,18 @@ public class Student {
         System.out.println("学生在学习！");
     }
 
-    /**
-     * 获取
-     * @return name
-     */
     public String getName() {
         return name;
     }
 
-    /**
-     * 设置
-     * @param name
-     */
     public void setName(String name) {
         this.name = name;
     }
 
-    /**
-     * 获取
-     * @return age
-     */
     public int getAge() {
         return age;
     }
 
-    /**
-     * 设置
-     * @param age
-     */
     public void setAge(int age) {
         this.age = age;
     }
