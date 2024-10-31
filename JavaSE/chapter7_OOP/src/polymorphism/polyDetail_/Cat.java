d4600e0453ce6c2219b8cb053c16914ca7033c03
Runze Yang <ryanyyyyyyy@163.com>
2024-10-31 09:29:51 +0800

    Revision on the polymorphism content update

--- JavaSE/chapter7_OOP/src/polymorphism/polyDetail_/Cat.java
+++ JavaSE/chapter7_OOP/src/polymorphism/polyDetail_/Cat.java
@@ -1,5 +1,9 @@
 package polymorphism.polyDetail_;
 
+/**
+ * @author ryanw
+ * */
+
 public class Cat extends Animal {
     public void eat(){//方法重写
         System.out.println("猫吃鱼");
--- JavaSE/chapter7_OOP/src/polymorphism/polyDetail_/PolyDetail02.java
+++ JavaSE/chapter7_OOP/src/polymorphism/polyDetail_/PolyDetail02.java
@@ -1,12 +1,22 @@
 package polymorphism.polyDetail_;
 
+/**
+ * @author ryanw
+ * */
+
 public class PolyDetail02 {
     public static void main(String[] args) {
-        //属性没有重写之说！属性的值看编译类型
-        Base base = new Sub();//向上转型
-        System.out.println(base.count);// ？ 看编译类型 10
-        Sub sub = new Sub();
-        System.out.println(sub.count);//?  20
+        // 属性没有重写之说！属性的值看编译类型
+        // 向上转型
+        Base base = new Sub();
+        System.out.println(base.count); // ? 看编译类型 10
+
+        // 向下转型
+        Sub sub = (Sub) base;
+        System.out.println(sub.count);
+
+//        Sub sub = new Sub();
+//        System.out.println(sub.count); // ?  20
     }
 }
 
