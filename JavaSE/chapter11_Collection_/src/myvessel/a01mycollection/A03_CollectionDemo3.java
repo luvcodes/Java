9c9f5605f510fafc474a142d24a65af8e4bb2846
Runze Yang <ryanyyyyyyy@163.com>
2024-11-04 21:47:18 +0800

    Update on Collection chapter and iterator method revision

--- JavaSE/chapter11_Collection_/src/myvessel/a01mycollection/A03_CollectionDemo3.java
+++ JavaSE/chapter11_Collection_/src/myvessel/a01mycollection/A03_CollectionDemo3.java
@@ -14,12 +14,6 @@ public class A03_CollectionDemo3 {
                 1.迭代器遍历
                 2.增强for遍历
                 3.lambda表达式遍历
-
-
-             迭代器遍历相关的三个方法：
-                    Iterator<E> iterator()  ：获取一个迭代器对象
-                    boolean hasNext()       ：判断当前指向的位置是否有元素
-                    E next()                ：获取当前指向的元素并移动指针
         */
 
         // 1. 创建集合并添加元素
@@ -34,6 +28,10 @@ public class A03_CollectionDemo3 {
         Iterator<String> iterator = coll.iterator();
 
         // 3. 利用循环不断的去获取集合中的每一个元素
+        //  迭代器遍历相关的三个方法：
+        //  Iterator<E> iterator()  ：获取一个迭代器对象
+        //  boolean hasNext()       ：判断当前指向的位置是否有元素
+        //  E next()                ：获取当前指向的元素并移动指针
         while (iterator.hasNext()) {
             // 4.next方法的两件事情：获取元素并移动指针
             /*String str = iterator.next();
