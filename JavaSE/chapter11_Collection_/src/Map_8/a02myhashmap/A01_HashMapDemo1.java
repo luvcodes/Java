023c997d53fe49466022b1d009bcaaa9c5678818
Runze Yang <ryanyyyyyyy@163.com>
2024-11-27 20:47:58 +0800

    Revision on the HashMap methods

--- JavaSE/chapter11_Collection_/src/Map_8/a02myhashmap/A01_HashMapDemo1.java
+++ JavaSE/chapter11_Collection_/src/Map_8/a02myhashmap/A01_HashMapDemo1.java
@@ -10,23 +10,17 @@ import java.util.Set;
 public class A01_HashMapDemo1 {
     public static void main(String[] args) {
        /*
-        需求：创建一个HashMap集合，键是学生对象(Student)，值是籍贯(String)。
-        存储三个键值对元素，并遍历
+        需求：创建一个HashMap集合，键是学生对象(Student)，值是籍贯(String)。存储三个键值对元素并遍历
         要求：同姓名，同年龄认为是同一个学生
-
         核心点：HashMap的键位置如果存储的是自定义对象，需要重写hashCode和equals方法。
        */
 
-        //1.创建HashMap的对象
         HashMap<Student, String> hm = new HashMap<>();
-
-        //2.创建三个学生对象
         Student s1 = new Student("zhangsan", 23);
         Student s2 = new Student("lisi", 24);
         Student s3 = new Student("wangwu", 25);
         Student s4 = new Student("wangwu", 25);
 
-        //3.添加元素
         hm.put(s1, "江苏");
         hm.put(s2, "浙江");
         hm.put(s3, "福建");
