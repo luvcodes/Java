40dda8fa6dc7b661434fca19f50cf55e67e894d9
Runze Yang <ryanyyyyyyy@163.com>
2024-11-28 21:05:30 +0800

    Revision on the HashMap methods

--- JavaSE/chapter11_Collection_/src/Map_8/a03mylinkedhashmap/A01_LinkedHashMapDemo1.java
+++ JavaSE/chapter11_Collection_/src/Map_8/a03mylinkedhashmap/A01_LinkedHashMapDemo1.java
@@ -14,16 +14,12 @@ public class A01_LinkedHashMapDemo1 {
                原理：底层数据结构是依然哈希表，只是每个键值对元素又额外的多了一个双链表的机制记录存储的顺序。
          */
 
-        //1.创建集合
-        LinkedHashMap<String, Integer> lhm = new LinkedHashMap<>();
-
-        //2.添加元素
+        LinkedHashMap<String, Integer> lhm = new LinkedHashMap<String, Integer>();
         lhm.put("c", 789);
         lhm.put("b", 456);
         lhm.put("a", 123);
         lhm.put("a", 111);
 
-        //3.打印集合
         System.out.println(lhm);
     }
 }
