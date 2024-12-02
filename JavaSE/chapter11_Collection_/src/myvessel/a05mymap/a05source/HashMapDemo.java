ecfe94fd1850c4dd3f127841e6bcb951ab756279
Runze Yang <ryanyyyyyyy@163.com>
2024-12-02 22:23:59 +0800

    Revise on hashmap and treemap

--- JavaSE/chapter11_Collection_/src/myvessel/a05mymap/a05source/HashMapDemo.java
+++ JavaSE/chapter11_Collection_/src/myvessel/a05mymap/a05source/HashMapDemo.java
@@ -4,16 +4,12 @@ import java.util.HashMap;
 
 public class HashMapDemo {
     public static void main(String[] args) {
-
-
-        HashMap<String,Integer> hm = new HashMap<>();
+        HashMap<String,Integer> hm = new HashMap<String,Integer>();
 
         hm.put("aaa" , 111);
         hm.put("bbb" , 222);
         hm.put("ccc" , 333);
         hm.put("ddd" , 444);
         hm.put("eee" , 555);
-
-
     }
 }
--- JavaSE/chapter11_Collection_/src/myvessel/a05mymap/a05source/TreeMapDemo.java
+++ JavaSE/chapter11_Collection_/src/myvessel/a05mymap/a05source/TreeMapDemo.java
@@ -4,13 +4,13 @@ import java.util.TreeMap;
 
 public class TreeMapDemo {
     public static void main(String[] args) {
-        TreeMap<Integer, String> tm = new TreeMap<>();
+        TreeMap<Integer, String> tm = new TreeMap<Integer, String>();
+
         tm.put(1, "aaa");
         tm.put(2, "bbb");
         tm.put(3, "ccc");
         tm.put(4, "ddd");
 
         System.out.println(tm);
-
     }
 }
